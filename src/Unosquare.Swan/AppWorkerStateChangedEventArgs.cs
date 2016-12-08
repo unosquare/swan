8a307204ae3468253ed8a6f92f9b95c34b9ddb61
Geo Perez <geovanni.perez@gmail.com>
2016-12-08 17:22:20 -0600

    Singleton, AppWorker and SettingsProvider

---
+++ src/Unosquare.Swan/AppWorkerStateChangedEventArgs.cs
@@ -0,0 +1,54 @@
+﻿namespace Unosquare.Swan
+{
+    using System;
+
+    /// <summary>
+    /// Enumerates the different Application Service States
+    /// </summary>
+    public enum AppWorkerState
+    {
+        /// <summary>
+        /// The stopped
+        /// </summary>
+        Stopped,
+        /// <summary>
+        /// The running
+        /// </summary>
+        Running,
+    }
+
+    /// <summary>
+    /// Represents event arguments whenever the state of an application service changes
+    /// </summary>
+    public class AppWorkerStateChangedEventArgs : EventArgs
+    {
+        /// <summary>
+        /// Gets the state to which the application service changed.
+        /// </summary>
+        public AppWorkerState NewState { get; private set; }
+
+        /// <summary>
+        /// Gets the old state.
+        /// </summary>
+        public AppWorkerState OldState { get; private set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AppWorkerStateChangedEventArgs" /> class.
+        /// </summary>
+        /// <param name="oldState">The old state.</param>
+        /// <param name="newState">The new state.</param>
+        public AppWorkerStateChangedEventArgs(AppWorkerState oldState, AppWorkerState newState)
+            : base()
+        {
+            this.OldState = oldState;
+            this.NewState = newState;
+        }
+    }
+
+    /// <summary>
+    /// An event handler that is called whenever the state of an application service is changed
+    /// </summary>
+    /// <param name="sender">The sender.</param>
+    /// <param name="e">The <see cref="AppWorkerStateChangedEventArgs"/> instance containing the event data.</param>
+    public delegate void ServiceStateChangedEventHandler(object sender, AppWorkerStateChangedEventArgs e);
+}
---
+++ src/Unosquare.Swan/WinServiceExtensions.cs
@@ -0,0 +1,53 @@
+﻿namespace Unosquare.Swan
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Reflection;
+    using System.ServiceProcess;
+    using System.Threading;
+
+    public static class WinServiceExtensions
+    {
+        private const string OnStartMethodName = "OnStart";
+        private const string OnStopMethodName = "OnStop";
+
+        public static void RunServicesInConsoleMode(this ServiceBase[] servicesToRun)
+        {
+            var onStartMethod = typeof(ServiceBase).GetMethod(OnStartMethodName,
+                BindingFlags.Instance | BindingFlags.NonPublic);
+            var onStopMethod = typeof(ServiceBase).GetMethod(OnStopMethodName,
+                BindingFlags.Instance | BindingFlags.NonPublic);
+            var serviceThreads = new List<Thread>();
+
+            // TODO: Log
+            //LogProvider.Current.Log("Starting services . . .");
+
+            foreach (var service in servicesToRun)
+            {
+                var thread = new Thread(() =>
+                {
+                    onStartMethod.Invoke(service, new object[] { new string[] { } });
+                    //LogProvider.Current.Log($"Started service '{service.GetType().Name}'");
+                });
+
+                serviceThreads.Add(thread);
+                thread.Start();
+            }
+
+            //LogProvider.Current.Log("Press any key to stop all services.");
+            Console.ReadLine();
+            //LogProvider.Current.Log("Stopping services . . .");
+
+            foreach (var service in servicesToRun)
+            {
+                onStopMethod.Invoke(service, null);
+                //LogProvider.Current.Log($"Stopped service '{service.GetType().Name}'");
+            }
+
+            foreach (var thread in serviceThreads)
+                thread.Join();
+
+            //LogProvider.Current.Log("Stopped all services.");
+        }
+    }
+}
