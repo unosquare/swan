50b39f62d65f8cd372a53957b1f55ee1e0e73b63
Geo Perez <geovanni.perez@gmail.com>
2016-12-09 14:05:27 -0600

    Initial unit testing

---
+++ test/Unosquare.Swan.Test/CurrentAppTest.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Unosquare.Swan.Test
+{
+    [TestFixture]
+    public class CurrentAppTest
+    {
+        [Test]
+        public void IsSingleInstanceTest()
+        {
+            Assert.IsTrue(CurrentApp.IsSingleInstance);
+        }
+
+        [Test]
+        public void IsOsDifferentUnknownTest()
+        {
+            Assert.AreNotEqual(CurrentApp.OS, Os.Unknown, $"Retrieving a OS: {CurrentApp.OS}");
+        }
+
+        [Test]
+        public void GetLocalStorageTest()
+        {
+            Assert.IsNotEmpty(CurrentApp.LocalStoragePath, $"Retrieving a local storage path: {CurrentApp.LocalStoragePath}");
+        }
+
+        [Test]
+        public void GetProcessTest()
+        {
+            Assert.IsNotNull(CurrentApp.Process);
+            Assert.AreEqual(CurrentApp.Process.ProcessName,
+#if NET452
+                "dotnet-test-nunit"
+#else
+                "dotnet"
+#endif
+                );
+        }
+        
+        [Test]
+        public void GetEntryAssemblyTest()
+        {
+            Assert.IsNotNull(CurrentApp.EntryAssembly);
+            Assert.IsTrue(CurrentApp.EntryAssembly.FullName.StartsWith("dotnet-test-nunit"));
+        }
+    }
+}
