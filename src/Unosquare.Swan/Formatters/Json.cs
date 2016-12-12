2b7b7989644dd4ac299a1f75d7a31f3460b85902
Geo Perez <geovanni.perez@gmail.com>
2016-12-12 13:18:16 -0600

    Basic array support

--- src/Unosquare.Swan/Formatters/Json.cs
+++ src/Unosquare.Swan/Formatters/Json.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -10,22 +12,22 @@ namespace Unosquare.Swan.Formatters
     {
         #region Constants 
 
-        const char InitialObjectCharacter = '{';
-        const char FinalObjectCharacter = '}';
+        private const char InitialObjectCharacter = '{';
+        private const char FinalObjectCharacter = '}';
 
-        const char InitialArrayCharacter = '[';
-        const char FinalArrayCharacter = ']';
+        private const char InitialArrayCharacter = '[';
+        private const char FinalArrayCharacter = ']';
 
-        const char FieldSeparatorCharacter = ',';
-        const char ValueSeparatorCharacter = ':';
+        private const char FieldSeparatorCharacter = ',';
+        private const char ValueSeparatorCharacter = ':';
 
-        const char StringEscapeCharacter = '\\';
-        const char StringQuotedCharacter = '"';
-        const char MinusNumberCharacter = '-';
+        private const char StringEscapeCharacter = '\\';
+        private const char StringQuotedCharacter = '"';
+        private const char MinusNumberCharacter = '-';
 
-        const string TrueValue = "true";
-        const string FalseValue = "false";
-        const string NullValue = "null";
+        private const string TrueValue = "true";
+        private const string FalseValue = "false";
+        private const string NullValue = "null";
 
         #endregion
 
@@ -39,7 +41,7 @@ namespace Unosquare.Swan.Formatters
         #region Enumerations
 
         /// <summary>
-        /// Defines the 3 different read states
+        /// Defines the 4 different read states
         /// for the parsing state machine
         /// </summary>
         private enum ReadState
@@ -51,6 +53,16 @@ namespace Unosquare.Swan.Formatters
         }
 
         #endregion
+
+        public static string Serialize(IEnumerable coll)
+        {
+            var firstItem = coll.Cast<object>().FirstOrDefault(x => x != null);
+            var quotedValues = firstItem is string;
+
+            return "[" +
+                   string.Join(FieldSeparatorCharacter.ToStringInvariant(),
+                       coll.Cast<object>().Select(x => quotedValues ? $"\"{x}\"" : x.ToStringInvariant())) + "]";
+        }
 
         public static string Serialize(object obj)
         {
@@ -75,7 +87,7 @@ namespace Unosquare.Swan.Formatters
                 }
                 else
                 {
-                    // fallback to string
+                    // fall-back to string
                     sb.Append($"\"{prop.Name}\" : \"{value}\", ");
                 }
             }
@@ -83,7 +95,7 @@ namespace Unosquare.Swan.Formatters
             if (sb.Length > 0)
                 sb.Remove(sb.Length - 2, 2);
 
-            return "{" + sb.ToString() + "}";
+            return $"{{{sb}}}";
         }
 
         public static object Deserialize(string source)
@@ -93,47 +105,145 @@ namespace Unosquare.Swan.Formatters
 
         public static T Deserialize<T>(string source)
         {
+            if (string.IsNullOrEmpty(source))
+                throw new ArgumentNullException(nameof(source));
+
+            switch (source[0])
+            {
+                case InitialObjectCharacter:
+                    return ParseObject<T>(source);
+                case InitialArrayCharacter:
+                    var genericArgs = typeof(T).GetTypeInfo().GetGenericArguments();
+                    if (genericArgs.Any())
+                        return (T) ParseArray(genericArgs[0], source);
+
+                    return default(T);
+                default:
+                    return default(T);
+            }
+        }
+
+        private static void SetPropertyValue<T>(PropertyInfo[] properties, string propertyName,
+            string propertyStringValue, T result)
+        {
+            if (string.IsNullOrWhiteSpace(propertyName)) return;
+
+            var targetProperty = properties.FirstOrDefault(p => p.Name.Equals(propertyName));
+
+            // Skip if the property is not found
+            if (targetProperty == null)
+                return;
+
+            // Parse and assign the basic type value to the property
+            try
+            {
+                if (propertyStringValue == null)
+                {
+                    targetProperty.SetValue(result, null);
+                }
+                else
+                {
+                    object propertyValue;
+                    if (Constants.BasicTypesInfo[targetProperty.PropertyType].TryParse(propertyStringValue,
+                        out propertyValue))
+                        targetProperty.SetValue(result, propertyValue);
+                }
+            }
+            catch
+            {
+                // swallow
+            }
+        }
+
+        private static IList ParseArray(Type type, string source)
+        {
+            var result = (IList) Activator.CreateInstance(typeof(List<>).MakeGenericType(type));
+            var currentState = ReadState.WaitingForValue;
+            var currentValue = new StringBuilder(1024);
+
+            for (var charIndex = 0; charIndex < source.Length; charIndex++)
+            {
+                // Get the current and next character
+                var currentChar = source[charIndex];
+                var nextChar = charIndex < source.Length - 1 ? source[charIndex + 1] : new char?();
+
+                // Perform logic based on state and decide on next state
+                switch (currentState)
+                {
+                    case ReadState.WaitingForValue:
+                    {
+                        if (currentChar == StringQuotedCharacter)
+                        {
+                            currentState = ReadState.PushingValue;
+                        }
+                        else if (currentChar == MinusNumberCharacter ||
+                                 char.IsNumber(currentChar))
+                        {
+                            currentState = ReadState.PushingValue;
+                            currentValue.Append(currentChar);
+                        }
+                        else if (currentChar == TrueValue[0] && nextChar.HasValue && nextChar == TrueValue[1])
+                        {
+                            result.Add(TrueValue);
+                            charIndex += TrueValue.Length;
+
+                            currentState = ReadState.WaitingForValue;
+                        }
+                        else if (currentChar == FalseValue[0] && nextChar.HasValue && nextChar == FalseValue[1])
+                        {
+                            result.Add(FalseValue);
+                            charIndex += FalseValue.Length;
+
+                            currentState = ReadState.WaitingForValue;
+                        }
+                        else if (currentChar == NullValue[0] && nextChar.HasValue && nextChar == NullValue[1])
+                        {
+                            result.Add(NullValue);
+                            charIndex += NullValue.Length;
+
+                            currentState = ReadState.WaitingForValue;
+                        }
+                        break;
+                    }
+                    case ReadState.PushingValue:
+                    {
+                        if (currentChar == StringQuotedCharacter ||
+                            currentChar == FieldSeparatorCharacter)
+                        {
+                            result.Add(currentValue.ToString());
+                            currentValue.Clear();
+                            currentState = ReadState.WaitingForValue;
+                        }
+                        else
+                        {
+                            currentValue.Append(currentChar);
+                        }
+                        break;
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        private static T ParseObject<T>(string source)
+        {
             var result = Activator.CreateInstance<T>();
+            PropertyInfo[] properties;
+
+            lock (SyncLock)
+            {
+                // Extract properties from cache
+                properties = TypeCache.Retrieve<T>(() =>
+                {
+                    return typeof(T).GetTypeInfo().GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                        .Where(x => x.CanWrite && Constants.BasicTypesInfo.ContainsKey(x.PropertyType));
+                });
+            }
+
             var currentState = ReadState.WaitingForNewField;
             var currentPropertyName = new StringBuilder(1024);
             var currentValue = new StringBuilder(1024);
-
-            // Extract properties from cache
-            var properties = TypeCache.Retrieve<T>(() =>
-            {
-                return typeof(T).GetTypeInfo().GetProperties(BindingFlags.Public | BindingFlags.Instance)
-                    .Where(x => x.CanWrite && Constants.BasicTypesInfo.ContainsKey(x.PropertyType));
-            });
-
-            var setPropertyAction = new Action<string, string, bool>((propertyName, propertyStringValue, isNull) =>
-            {
-                if (string.IsNullOrWhiteSpace(propertyName)) return;
-
-                var targetProperty = properties.FirstOrDefault(p => p.Name.Equals(propertyName));
-
-                // Skip if the property is not found
-                if (targetProperty == null)
-                    return;
-
-                // Parse and assign the basic type value to the property
-                try
-                {
-                    if (isNull)
-                    {
-                        targetProperty.SetValue(result, null);
-                    }
-                    else
-                    {
-                        object propertyValue = null;
-                        if (Constants.BasicTypesInfo[targetProperty.PropertyType].TryParse(propertyStringValue, out propertyValue))
-                            targetProperty.SetValue(result, propertyValue);
-                    }
-                }
-                catch
-                {
-                    // swallow
-                }
-            });
 
             for (var charIndex = 0; charIndex < source.Length; charIndex++)
             {
@@ -145,80 +255,80 @@ namespace Unosquare.Swan.Formatters
                 switch (currentState)
                 {
                     case ReadState.WaitingForNewField:
+                    {
+                        // clean up
+                        SetPropertyValue(properties, currentPropertyName.ToString(), currentValue.ToString(), result);
+                        currentPropertyName.Clear();
+                        currentValue.Clear();
+
+                        if (currentChar == StringQuotedCharacter)
                         {
-                            // clean up
-                            setPropertyAction(currentPropertyName.ToString(), currentValue.ToString(), false);
-                            currentPropertyName.Clear();
-                            currentValue.Clear();
-
-                            if (currentChar == InitialObjectCharacter) continue;
-
-                            if (currentChar == StringQuotedCharacter)
-                            {
-                                currentState = ReadState.PushingFieldName;
-                            }
-                            break;
+                            currentState = ReadState.PushingFieldName;
                         }
+                        break;
+                    }
                     case ReadState.PushingFieldName:
+                    {
+                        if (currentChar == StringQuotedCharacter)
                         {
-                            if (currentChar == StringQuotedCharacter)
-                            {
-                                currentState = ReadState.WaitingForValue;
-                            }
-                            else
-                            {
-                                currentPropertyName.Append(currentChar);
-                            }
-                            break;
+                            currentState = ReadState.WaitingForValue;
                         }
+                        else
+                        {
+                            currentPropertyName.Append(currentChar);
+                        }
+                        break;
+                    }
                     case ReadState.WaitingForValue:
+                    {
+                        if (currentChar == StringQuotedCharacter)
                         {
-                            if (currentChar == StringQuotedCharacter)
-                            {
-                                currentState = ReadState.PushingValue;
-                            }
-                            else if (currentChar == MinusNumberCharacter ||
-                                char.IsNumber(currentChar))
-                            {
-                                currentState = ReadState.PushingValue;
-                                currentValue.Append(currentChar);
-                            }
-                            else if (currentChar == TrueValue[0] && nextChar.HasValue && nextChar == TrueValue[1])
-                            {
-                                setPropertyAction(currentPropertyName.ToString(), true.ToStringInvariant(), false);
-                                charIndex += TrueValue.Length;
-
-                                currentState = ReadState.WaitingForNewField;
-                            }
-                            else if (currentChar == FalseValue[0] && nextChar.HasValue && nextChar == FalseValue[1])
-                            {
-                                setPropertyAction(currentPropertyName.ToString(), false.ToStringInvariant(), false);
-                                charIndex += FalseValue.Length;
-
-                                currentState = ReadState.WaitingForNewField;
-                            }
-                            else if (currentChar == NullValue[0] && nextChar.HasValue && nextChar == NullValue[1])
-                            {
-                                setPropertyAction(currentPropertyName.ToString(), null, true);
-                                charIndex += NullValue.Length;
-
-                                currentState = ReadState.WaitingForNewField;
-                            }
-                            break;
+                            currentState = ReadState.PushingValue;
                         }
+                        else if (currentChar == MinusNumberCharacter ||
+                                 char.IsNumber(currentChar))
+                        {
+                            currentState = ReadState.PushingValue;
+                            currentValue.Append(currentChar);
+                        }
+                        else if (currentChar == TrueValue[0] && nextChar.HasValue && nextChar == TrueValue[1])
+                        {
+                            SetPropertyValue(properties, currentPropertyName.ToString(), true.ToStringInvariant(),
+                                result);
+                            charIndex += TrueValue.Length;
+
+                            currentState = ReadState.WaitingForNewField;
+                        }
+                        else if (currentChar == FalseValue[0] && nextChar.HasValue && nextChar == FalseValue[1])
+                        {
+                            SetPropertyValue(properties, currentPropertyName.ToString(), false.ToStringInvariant(),
+                                result);
+                            charIndex += FalseValue.Length;
+
+                            currentState = ReadState.WaitingForNewField;
+                        }
+                        else if (currentChar == NullValue[0] && nextChar.HasValue && nextChar == NullValue[1])
+                        {
+                            SetPropertyValue(properties, currentPropertyName.ToString(), null, result);
+                            charIndex += NullValue.Length;
+
+                            currentState = ReadState.WaitingForNewField;
+                        }
+                        break;
+                    }
                     case ReadState.PushingValue:
+                    {
+                        if (currentChar == StringQuotedCharacter ||
+                            currentChar == FieldSeparatorCharacter)
                         {
-                            if (currentChar == StringQuotedCharacter || 
-                                currentChar == FieldSeparatorCharacter)
-                            {
-                                currentState = ReadState.WaitingForNewField;
-                            }
-                            else
-                            {
-                                currentValue.Append(currentChar);
-                            }
-                            break;
+                            currentState = ReadState.WaitingForNewField;
                         }
+                        else
+                        {
+                            currentValue.Append(currentChar);
+                        }
+                        break;
+                    }
                 }
             }
 
@@ -230,11 +340,11 @@ namespace Unosquare.Swan.Formatters
             lock (SyncLock)
             {
                 return TypeCache.Retrieve(type, () =>
-                    {
-                        return type.GetTypeInfo().GetProperties(BindingFlags.Public | BindingFlags.Instance)
-                            .Where(p => p.CanRead)
-                            .ToArray();
-                    }).ToArray();
+                {
+                    return type.GetTypeInfo().GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                        .Where(p => p.CanRead)
+                        .ToArray();
+                }).ToArray();
             }
         }
     }
--- test/Unosquare.Swan.Test/Program.cs
+++ test/Unosquare.Swan.Test/Program.cs
@@ -12,27 +12,46 @@ namespace Unosquare.Swan.Test
     {
         public static void Main(string[] args)
         {
+            #region basic obj
+
             var basicObj = new BasicJson { StringData = "string", IntData = 1, NegativeInt = -1, DecimalData = 10.33M, BoolData = true };
             var basicStr = "{\"StringData\" : \"string\", \"IntData\" : 1, \"NegativeInt\" : -1, \"DecimalData\" : 10.33, \"BoolData\" : true, \"StringNull\" : null}";
 
             var data = Json.Serialize(basicObj);
             data.Info();
 
-            if (data == basicStr) "Cool serialize".Info();
+            if (data == basicStr) "Ok serialize".Info(); else "Error serialize".Error();
 
             var obj = Json.Deserialize<BasicJson>(basicStr);
+
+            if (obj.StringData == basicObj.StringData) "Ok string".Info(); else "Error string".Error();
+
+            if (obj.IntData == basicObj.IntData) "Ok int".Info(); else "Error int".Error();
+
+            if (obj.NegativeInt == basicObj.NegativeInt) "Ok neg int".Info(); else "Error neg int".Error();
+
+            if (obj.BoolData == basicObj.BoolData) "Ok bool".Info(); else "Error bool".Error();
+
+            if (obj.DecimalData == basicObj.DecimalData) "Ok decimal".Info(); else "Error decimal".Error();
+
+            if (obj.StringNull == basicObj.StringNull) "Ok null".Info(); else "Error null".Error();
+
+            #endregion
+
+            #region basic array
+
+            var basicArray = new[] { "One", "Two", "Three" };
+            var basicAStr = "[\"One\",\"Two\",\"Three\"]";
+            var dataArr = Json.Serialize(basicArray);
+            dataArr.Info();
+
+            if (dataArr == basicAStr) "Ok serialize".Info(); else "Error serialize".Error();
+
+            var arr = Json.Deserialize<List<string>>(basicAStr);
             
-            if (obj.StringData == basicObj.StringData) "Cool string".Info();
+            if (string.Join(",", basicArray) == string.Join(",", arr)) "Ok array".Info(); else "Error array".Error();
 
-            if (obj.IntData == basicObj.IntData) "Cool int".Info();
-
-            if (obj.NegativeInt == basicObj.NegativeInt) "Cool neg int".Info();
-
-            if (obj.BoolData == basicObj.BoolData) "Cool bool".Info();
-
-            if (obj.DecimalData == basicObj.DecimalData) "Cool decimal".Info();
-
-            if (obj.StringNull == basicObj.StringNull) "Cool null".Info();
+            #endregion
 
             Console.ReadLine();
         }
