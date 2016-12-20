b79566d17af040633e538cd553c5fa743d5e980a
Geo Perez <geovanni.perez@gmail.com>
2016-12-20 15:56:31 -0600

    Fixing issues with SettingsProvider

--- test/Unosquare.Swan.Test/SettingsProviderTest.cs
+++ test/Unosquare.Swan.Test/SettingsProviderTest.cs
@@ -1,9 +1,6 @@
 ﻿using NUnit.Framework;
-using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
 using Unosquare.Swan.Abstractions;
 using Unosquare.Swan.Test.Mocks;
 
@@ -12,7 +9,7 @@ namespace Unosquare.Swan.Test
     [TestFixture]
     public class SettingsProviderTest
     {
-        private readonly SettingsProvider<AppSettingMock> mock = new SettingsProvider<AppSettingMock>()
+        private readonly SettingsProvider<AppSettingMock> _mock = new SettingsProvider<AppSettingMock>()
         {
             ConfigurationFilePath = Path.GetTempFileName()
         };
@@ -20,14 +17,25 @@ namespace Unosquare.Swan.Test
         [Test]
         public void TryGlobalTest()
         {
-            Assert.IsNotNull(mock.Global);
-            Assert.IsNotNull(mock.Global.WebServerHostname);
-            Assert.IsNotNull(mock.Global.WebServerPort);
+            Assert.IsNotNull(_mock.Global);
+            Assert.IsNotNull(_mock.Global.WebServerHostname);
+            Assert.IsNotNull(_mock.Global.WebServerPort);
 
             var appSettings = new AppSettingMock();
 
-            Assert.AreEqual(appSettings.WebServerHostname, mock.Global.WebServerHostname);
-            Assert.AreEqual(appSettings.WebServerPort, mock.Global.WebServerPort);
+            Assert.AreEqual(appSettings.WebServerHostname, _mock.Global.WebServerHostname);
+            Assert.AreEqual(appSettings.WebServerPort, _mock.Global.WebServerPort);
+        }
+
+        [Test]
+        public void GetListTest()
+        {
+            Assert.IsNotNull(_mock.GetList());
+
+            Assert.AreEqual(3, _mock.GetList().Count);
+            Assert.AreEqual(typeof(int).Name, _mock.GetList().First().DataType);
+            Assert.AreEqual("WebServerPort", _mock.GetList().First().Property);
+            Assert.AreEqual(9898, _mock.GetList().First().DefaultValue);
         }
     }
 }
