aaf8ccd273281e9d3e4a9f2ce3eeb585ce17b79b
Geo Perez <geovanni.perez@gmail.com>
2016-12-21 13:53:46 -0600

    Terminal unit test

--- test/Unosquare.Swan.Test/TerminalTest.cs
+++ test/Unosquare.Swan.Test/TerminalTest.cs
@@ -1,4 +1,9 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Unosquare.Swan.Test.Mocks;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Unosquare.Swan.Test
 {
@@ -11,12 +16,45 @@ namespace Unosquare.Swan.Test
             if (CurrentApp.OS == OperatingSystem.Windows)
             {
                 // Funny, the console is not here :P
-                Assert.IsFalse(Terminal.IsConsolePresent); 
+                Assert.IsFalse(Terminal.IsConsolePresent);
             }
             else
             {
                 Assert.IsTrue(Terminal.IsConsolePresent);
             }
+        }
+
+        [Test]
+        public void LoggingTest()
+        {
+            var messages = new List<LoggingEntryMock>();
+
+            Terminal.Settings.OnMessageLogged = (s, t, d, src, m, ex) =>
+            {
+                messages.Add(new LoggingEntryMock
+                {
+                    DateTime = d,
+                    Exception = ex,
+                    Message = m,
+                    Source = src,
+                    Type = t
+                });
+            };
+
+            nameof(LoggingMessageType.Info).Info();
+            nameof(LoggingMessageType.Debug).Debug();
+            nameof(LoggingMessageType.Error).Error();
+            nameof(LoggingMessageType.Trace).Trace();
+            nameof(LoggingMessageType.Warning).Warn();
+
+            Assert.IsTrue(messages.All(x => x.Message == x.Type.ToString()));
+
+            new Exception().Error(nameof(TerminalTest), nameof(LoggingTest));
+            Task.Delay(100).Wait();
+
+            Assert.IsTrue(messages.Any(x => x.Exception != null));
+            Assert.IsTrue(messages.Any(x => x.Source == nameof(TerminalTest)));
+            Assert.IsTrue(messages.Any(x => x.Message == nameof(LoggingTest)));
         }
     }
 }
