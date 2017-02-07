a5bb96d6828e9242e7dcd0fed5e31c6d41663c00
chubbsnes65 <nestor.soto@unosquare.com>
2017-02-07 13:21:08 -0600

    Add BusinessRuleAttribute, BusinessRulesControllers, IAuditTrailEntry

---
+++ src/Unosquare.Swan.AspNetCore/BusinessRulesControllers.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Unosquare.Swan.AspNetCore
+{
+    /// <summary>
+    /// Represents a Business Rules Controller
+    /// </summary>
+    public interface IBusinessRulesController
+    {
+        /// <summary>
+        /// Runs the business rules.
+        /// </summary>
+        void RunBusinessRules();
+    }
+
+    /// <summary>
+    /// Creates a new DbContext with support to run BusinessControllers
+    /// </summary>
+    public interface IBusinessDbContext
+    {
+        /// <summary>
+        /// Adds the controller.
+        /// </summary>
+        /// <param name="controller">The controller.</param>
+        void AddController(IBusinessRulesController controller);
+        /// <summary>
+        /// Removes the controller.
+        /// </summary>
+        /// <param name="controller">The controller.</param>
+        void RemoveController(IBusinessRulesController controller);
+        /// <summary>
+        /// Determines whether the specified controller contains controller.
+        /// </summary>
+        /// <param name="controller">The controller.</param>
+        /// <returns>
+        ///   <c>true</c> if the specified controller contains controller; otherwise, <c>false</c>.
+        /// </returns>
+        bool ContainsController(IBusinessRulesController controller);
+    }
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <seealso cref="Unosquare.Swan.AspNetCore.IBusinessRulesController" />
+    public abstract class BusinessRulesControllers<T> : IBusinessRulesController 
+        where T : DbContext
+    {
+        const string DynamicProxiesNamespace = "Microsoft.EntityFrameworkCore";
+        /// <summary>
+        /// Gets or sets the context.
+        /// </summary>
+        /// <value>
+        /// The context.
+        /// </value>
+        public T Context { get; protected set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BusinessRulesControllers{T}"/> class.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        protected BusinessRulesControllers(T context)
+        {
+            Context = context;
+        }
+
+        /// <summary>
+        /// Runs the business rules.
+        /// </summary>
+        public void RunBusinessRules()
+        {
+            var methodInfoSet = GetType().GetMethods().Where(m => m.ReturnType == typeof(void) && m.IsPublic
+                                                                                    && !m.IsConstructor &&
+                                                                                    m.GetCustomAttributes(typeof(BusinessRuleAttribute),
+                                                                                    true).Any()).ToArray();
+            ExecuteBusinessRulesMethods(EntityState.Added, ActionFlags.Create, methodInfoSet);
+            ExecuteBusinessRulesMethods(EntityState.Modified, ActionFlags.Update, methodInfoSet);
+            ExecuteBusinessRulesMethods(EntityState.Deleted, ActionFlags.Delete, methodInfoSet);
+        }
+
+        /// <summary>
+        /// Gets the type of the entity.
+        /// </summary>
+        /// <param name="entity">The entity.</param>
+        /// <returns></returns>
+        public Type GetEntityType(object entity)
+        {
+            var entityType = entity.GetType();
+
+            if (entityType.BaseType() != null && entityType.Namespace == DynamicProxiesNamespace)
+                entityType = entityType.BaseType();
+
+            return entityType;
+        }    
+
+        private void ExecuteBusinessRulesMethods(EntityState state, ActionFlags action, MethodInfo[] methodInfoSet)
+        {
+            var selfTrackingEntries = Context.ChangeTracker.Entries().Where(x => x.State == state);
+
+            foreach(var entry in selfTrackingEntries)
+            {
+                var entity = entry.Entity;
+                if (entry == null) continue;
+                var entityType = entity.GetType();
+
+                if (entityType.BaseType() != null && entityType.Namespace == DynamicProxiesNamespace)
+                    entityType = entityType.BaseType();
+
+                var methods = methodInfoSet.Where(m => m.GetCustomAttributes(typeof (BusinessRuleAttribute), true)
+                .Select(a => a as BusinessRuleAttribute)
+                .Any(
+                    b => (b.EntityTypes == null ||
+                          b.EntityTypes.Any(t => t == entityType)) &&
+                          (b.Action & action) == action));
+
+                foreach (var methodInfo in methods)
+                {
+                    methodInfo.Invoke(this, new[] { entity });
+                }
+            }
+        }
+    }
+}
---
+++ src/Unosquare.Swan.AspNetCore/Models/IAuditTrailEntry.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Unosquare.Swan.AspNetCore.Models
+{
+    /// <summary>
+    /// Represents an AuditTrail Entry
+    /// </summary>
+    public interface IAuditTrailEntry
+    {
+        /// <summary>
+        /// Gets or sets the user identifier.
+        /// </summary>
+        /// <value>
+        /// The user identifier.
+        /// </value>
+        string UserId { get; set; }
+        /// <summary>
+        /// Gets or sets the name of the table.
+        /// </summary>
+        /// <value>
+        /// The name of the table.
+        /// </value>
+        string TableName { get; set; }
+        /// <summary>
+        /// Gets or sets the action.
+        /// </summary>
+        /// <value>
+        /// The action.
+        /// </value>
+        int Action { get; set; }
+        /// <summary>
+        /// Gets or sets the json body.
+        /// </summary>
+        /// <value>
+        /// The json body.
+        /// </value>
+        string JsonBody { get; set; }
+        /// <summary>
+        /// Gets or sets the date created.
+        /// </summary>
+        /// <value>
+        /// The date created.
+        /// </value>
+        DateTime DateCreated { get; set; }
+    }
+}
