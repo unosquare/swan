590f7024934ecc2711fb6ed4ea6671b1bb92e007
chubbsnes65 <nestor.soto@unosquare.com>
2017-02-09 12:57:07 -0600

    Changes in ExecuteBusinessRulesMethods

--- src/Unosquare.Swan.AspNetCore/BusinessRulesController.cs
+++ src/Unosquare.Swan.AspNetCore/BusinessRulesController.cs
@@ -105,9 +105,9 @@ namespace Unosquare.Swan.AspNetCore
 
                 var methods = methodInfoSet.Where(m => m.GetCustomAttributes(typeof(BusinessRuleAttribute), true)
                     .Select(a => a as BusinessRuleAttribute)
-                    .Any(b => b.EntityTypes == null ||
-                              b.EntityTypes.Any(t => t == entityType) &&
-                              (b.Action & action) == action));
+                    .Any(b => (b.EntityTypes == null ||
+                               b.EntityTypes.Any(t => t == entityType)) &&
+                               b.Action == action));
 
                 foreach (var methodInfo in methods)
                 {
