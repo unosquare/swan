37a9bdf1a3700586e1791b51b4b2e2598a790dbb
greciaveronica <greciaveronica@gmail.com>
2017-02-24 13:06:01 -0600

    Fix ToSafeFilenameTest

--- test/Unosquare.Swan.Test/ExtensionsStringTest.cs
+++ test/Unosquare.Swan.Test/ExtensionsStringTest.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using System;
+using System.Runtime.InteropServices;
 using Unosquare.Swan.Test.Mocks;
 
 namespace Unosquare.Swan.Test
@@ -105,7 +106,18 @@ namespace Unosquare.Swan.Test
         [TestCase("LongFileNameLongFileNameLongFileNameLongFileNameLongFileNameLongFileNameLongFileNameLongFileNameLongFileNameLongFileNameLongFileNameLongFileNameLongFileNameLongFileNameLongFileNameLongFileNameLongFileNameLongFileNameLong", "LongFileNameLongFileNameLongFileNameLongFileNameLongFileNameLongFileNameLongFileNameLongFileNameLongFileNameLongFileNameLongFileNameLongFileNameLongFileNameLongFileNameLongFileNameLongFileNameLongFileNameLongFileNameLongFileName")]
         public void ToSafeFilenameTest(string expected, string input)
         {
+            var isWindows = false;
+
+#if NET452
+            isWindows = System.Environment.OSVersion.ToString().Contains("Windows");
+#endif
+#if NETCOREAPP1_1
+            isWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+#endif
+
+            Assert.Ignore("Ignored",!isWindows);
             Assert.AreEqual(expected, input.ToSafeFilename(), $"Testing with {input}");
+
         }
 
         [Test]
