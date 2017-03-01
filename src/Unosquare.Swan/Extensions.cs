3226e5b9a64474d8b1a1279371bfb1e8897fcdc0
Geo Perez <geovanni.perez@gmail.com>
2017-03-01 15:28:07 -0600

    Add CopyPropertiesTo for Dictionaries

--- src/Unosquare.Swan/Extensions.cs
+++ src/Unosquare.Swan/Extensions.cs
@@ -1,4 +1,6 @@
-﻿namespace Unosquare.Swan
+﻿using System.Collections.Generic;
+
+namespace Unosquare.Swan
 {
     using Reflection;
     using System;
@@ -90,6 +92,79 @@
                     var sourceStringValue = sourceProperty.GetValue(source).ToStringInvariant();
                     object targetValue;
                     if (Definitions.BasicTypesInfo[targetProperty.PropertyType].TryParse(sourceStringValue, out targetValue))
+                    {
+                        targetProperty.SetValue(target, targetValue);
+                        copiedProperties++;
+                    }
+                }
+                catch
+                {
+                    // swallow
+                }
+            }
+
+            return copiedProperties;
+        }
+
+        /// <summary>
+        /// Iterates over the keys of the source and tries to write a compatible value to a public, 
+        /// instance, writable property in the destination.
+        /// 
+        /// This method only supports basic types and it is not multi level
+        /// </summary>
+        /// <param name="source">The source.</param>
+        /// <param name="target">The target.</param>
+        /// <param name="ignoreProperties">The ignore properties.</param>
+        /// <returns></returns>
+        public static int CopyPropertiesTo(this IDictionary<string, object> source, object target,
+            string[] ignoreProperties)
+        {
+            var copyPropertiesTargets = new Lazy<PropertyTypeCache>(() => new PropertyTypeCache());
+
+            var copiedProperties = 0;
+
+            var targetType = target.GetType();
+            var targetProperties = copyPropertiesTargets.Value.Retrieve(targetType, () =>
+            {
+                return targetType.GetTypeInfo().GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                    .Where(x => x.CanWrite && Definitions.AllBasicTypes.Contains(x.PropertyType));
+            });
+
+            var targetPropertyNames = targetProperties.Select(t => t.Name.ToLowerInvariant());
+            var filteredSourceKeys = source
+                .Where(s => targetPropertyNames.Contains(s.Key.ToLowerInvariant()) && s.Value != null)
+                .ToArray();
+
+            var ignoredProperties = ignoreProperties?.Where(p => string.IsNullOrWhiteSpace(p) == false)
+                                        .Select(p => p.ToLowerInvariant())
+                                        .ToArray() ?? new string[] { };
+
+            foreach (var sourceKey in filteredSourceKeys)
+            {
+                var targetProperty =
+                    targetProperties.SingleOrDefault(s => s.Name.ToLowerInvariant() == sourceKey.Key.ToLowerInvariant());
+                if (targetProperty == null) continue;
+
+                if (ignoredProperties.Contains(targetProperty.Name.ToLowerInvariant()))
+                    continue;
+
+                try
+                {
+                    if (targetProperty.PropertyType == sourceKey.Value.GetType())
+                    {
+                        targetProperty.SetValue(target, sourceKey.Value);
+                        copiedProperties++;
+                        continue;
+                    }
+
+                    var sourceStringValue = sourceKey.Value.ToStringInvariant();
+
+                    if (targetProperty.PropertyType == typeof(bool))
+                        sourceStringValue = sourceStringValue == "1" ? "true" : "false";
+
+                    object targetValue;
+                    if (Definitions.BasicTypesInfo[targetProperty.PropertyType].TryParse(sourceStringValue,
+                        out targetValue))
                     {
                         targetProperty.SetValue(target, targetValue);
                         copiedProperties++;
