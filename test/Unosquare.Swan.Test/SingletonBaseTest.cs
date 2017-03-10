c286b080f61c3a6c5493278d2036adfa9bbad4e4
greciaveronica <greciaveronica@gmail.com>
2017-03-10 15:18:25 -0600

    Completing SingletonBase tests

--- test/Unosquare.Swan.Test/SingletonBaseTest.cs
+++ test/Unosquare.Swan.Test/SingletonBaseTest.cs
@@ -10,10 +10,43 @@ namespace Unosquare.Swan.Test
     [TestFixture]
     public class SingletonBaseTest
     {
+        static MockProvider mocks = new MockProvider();
+        static MockProvider mocksInstance = MockProvider.Instance;
+
         [Test]
         public void GetInstanceNameTest()
         {
-            Assert.AreEqual(nameof(MockProvider), MockProvider.Instance.GetName());
+            Assert.AreEqual(nameof(MockProvider), mocksInstance.GetName());
+        }
+
+        [Test]
+        public void GetTypeTest()
+        {
+            Assert.AreEqual(typeof(MockProvider), mocksInstance.GetType());
+        }
+
+        [Test]
+        public void StringifyTest()
+        {
+            Assert.AreEqual(mocks.Stringify(), mocksInstance.Stringify());
+        }
+
+        [Test]
+        public void ToJsonTest()
+        {
+            Assert.AreEqual(mocks.ToJson(), mocksInstance.ToJson());
+        }
+
+        [Test]
+        public void ToStringTest()
+        {
+            Assert.AreEqual(mocks.ToString(), mocksInstance.ToString());
+        }
+
+        [Test]
+        public void ToStringInvariantTest()
+        {
+            Assert.AreEqual(mocks.ToStringInvariant(), mocksInstance.ToStringInvariant());
         }
     }
 }
