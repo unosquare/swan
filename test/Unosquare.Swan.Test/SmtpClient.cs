7ec14367c4b5908e7232398dc98b2e79fe0dde82
Geo Perez <geovanni.perez@gmail.com>
2017-03-14 10:05:49 -0600

    Add more unit tests

---
+++ test/Unosquare.Swan.Test/SmtpClient.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Unosquare.Swan.Networking;
+
+namespace Unosquare.Swan.Test
+{
+    [TestFixture]
+    public class SnmpClieSmtpClientntTest
+    {
+        [Test]
+        public void TestConnectGmailSmtpException()
+        {
+            Assert.ThrowsAsync<SmtpException>(async () =>
+            {
+                var client = new SmtpClient("smtp.gmail.com", 587);
+
+                await client.SendMailAsync(new SmtpSessionState());
+            });
+        }
+    }
+}
---
+++ test/Unosquare.Swan.Test/SnmpClientTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using Unosquare.Swan.Networking;
+
+namespace Unosquare.Swan.Test
+{
+    [TestFixture]
+    public class SnmpClientTest
+    {
+        [Test]
+        public void TestDiscovery()
+        {
+            var data = SnmpClient.Discover();
+            Assert.IsNotNull(data);
+            Assert.IsTrue(data.Any());
+        }
+    }
+}
