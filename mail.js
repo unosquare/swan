eec2cccafec65a7896e27053ce1d74d9123b5bfc
Geo Perez <geovanni.perez@gmail.com>
2017-07-24 16:36:13 -0500

    Trying to bind only to localhost

--- mail.js
+++ mail.js
@@ -4,11 +4,12 @@ const os = require('os');
 const path = require('path');
 
 const maildev = new MailDev({
-  smtp: 1030
+  smtp: 1030,
+  ip: '127.0.0.1'
 })
 
 maildev.listen(function(err) {
-  console.log('We can now sent emails to port 1025!')
+  console.log('We can now sent emails to port 1030!')
 })
 
 // Print new emails to the console as they come in
