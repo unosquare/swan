611f39334d51d8342c701931a8834ddb5788fc1f
Geo Perez <geovanni.perez@gmail.com>
2017-07-25 09:01:49 -0500

    Extend SmtpClient test

---
+++ test/Unosquare.Swan.Test/README.md
@@ -0,0 +1,10 @@
+﻿# Unosquare Swan Unit Tests
+
+## Notes
+
+If you want to run all the test you need to bring up the SMTP Dev server (MailDev). You need Node.js (probably you already have it) and install maildev to run the mail.js script.
+
+``
+# npm install maildev
+# node ./mail.js
+``
--- test/Unosquare.Swan.Test/SmtpClient.cs
+++ test/Unosquare.Swan.Test/SmtpClient.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.IO;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using Unosquare.Swan.Formatters;
 using Unosquare.Swan.Networking;
+using Unosquare.Swan.Test.Mocks;
 
 namespace Unosquare.Swan.Test
 {
@@ -30,25 +32,35 @@ namespace Unosquare.Swan.Test
         [Test]
         public async Task SendLocalEmail()
         {
+            const string senderEmail = "test@test.com";
+            const string recipientEmail = "me@test.com";
+
             if (Environment.GetEnvironmentVariable("APPVEYOR") == "True")
                 Assert.Inconclusive("Can not test in AppVeyor");
 
             var filename = Path.Combine(Path.GetTempPath(), "tempFile.msg");
+
             if (File.Exists(filename))
                 File.Delete(filename);
 
             Assert.IsFalse(File.Exists(filename));
             var email = new SmtpClient("localhost", 1030);
-            var session = new SmtpSessionState() {
-                SenderAddress = "test@test.com",
-                
-            };
-            session.Recipients.Add("test@test.com");
-            session.DataBuffer.AddRange(new byte[] { 0x48, 0x48, 0x0A, 0x0C });
+            var session = new SmtpSessionState {SenderAddress = senderEmail};
+
+            session.Recipients.Add(recipientEmail);
+            session.DataBuffer.AddRange(new byte[] {0x48, 0x48, 0x0A, 0x0C});
 
             await email.SendMailAsync(session);
             await Task.Delay(100);
             Assert.IsTrue(File.Exists(filename));
+
+            var smtpMock = Json.Deserialize<SmtpMock>(File.ReadAllText(filename));
+            Assert.IsNotNull(smtpMock);
+
+            Assert.AreEqual(senderEmail, smtpMock.envelope.from.address);
+            Assert.AreEqual(recipientEmail, smtpMock.envelope.to.First().address);
+
+            Assert.AreEqual("hh", smtpMock.headers.First().Key);
         }
     }
 }
