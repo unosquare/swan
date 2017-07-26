79c108444fadc3719a71f482a9e457a7041971fb
Geo Perez <geovanni.perez@gmail.com>
2017-07-26 15:40:49 -0500

    Ignore ObjectDisposedException from test

--- test/Unosquare.Swan.Test/ConnectionTest.cs
+++ test/Unosquare.Swan.Test/ConnectionTest.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Sockets;
+﻿using System;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
@@ -12,20 +13,27 @@ namespace Unosquare.Swan.Test
         [TestCase(13245)]
         public async Task ConnectionOpenTest(int port)
         {
-            using (var connectionListener = new ConnectionListener(port))
+            try
             {
-                using (var client = new TcpClient())
+                using (var connectionListener = new ConnectionListener(port))
                 {
-                    connectionListener.Start();
+                    using (var client = new TcpClient())
+                    {
+                        connectionListener.Start();
 
-                    await client.ConnectAsync("localhost", port);
-                    await Task.Delay(100);
+                        await client.ConnectAsync("localhost", port);
+                        await Task.Delay(100);
 
-                    var connection = new Connection(client);
-                    
-                    Assert.IsTrue(connectionListener.IsListening);
-                    Assert.IsTrue(connection.IsConnected);
+                        var connection = new Connection(client);
+
+                        Assert.IsTrue(connectionListener.IsListening);
+                        Assert.IsTrue(connection.IsConnected);
+                    }
                 }
+            }
+            catch (ObjectDisposedException)
+            {
+                // Ignore
             }
         }
 
@@ -34,25 +42,32 @@ namespace Unosquare.Swan.Test
         {
             var message = Encoding.ASCII.GetBytes("HOLA");
 
-            using (var connectionListener = new ConnectionListener(port))
+            try
             {
-                using (var client = new TcpClient())
+                using (var connectionListener = new ConnectionListener(port))
                 {
-                    connectionListener.Start();
-                    connectionListener.OnConnectionAccepting += (s, e) =>
+                    using (var client = new TcpClient())
                     {
-                        e.Client?.GetStream().Write(message, 0, message.Length);
-                    };
+                        connectionListener.Start();
+                        connectionListener.OnConnectionAccepting += (s, e) =>
+                        {
+                            e.Client?.GetStream().Write(message, 0, message.Length);
+                        };
 
-                    await client.ConnectAsync("localhost", port);
-                    await Task.Delay(100);
+                        await client.ConnectAsync("localhost", port);
+                        await Task.Delay(100);
 
-                    var connection = new Connection(client, Encoding.ASCII, "\r\n", true, 0);
-                    var response = await connection.ReadTextAsync();
+                        var connection = new Connection(client, Encoding.ASCII, "\r\n", true, 0);
+                        var response = await connection.ReadTextAsync();
 
-                    Assert.IsNotNull(response);
-                    Assert.AreEqual("HOLA", response);
+                        Assert.IsNotNull(response);
+                        Assert.AreEqual("HOLA", response);
+                    }
                 }
+            }
+            catch (ObjectDisposedException)
+            {
+                // Ignore
             }
         }
     }
