90c5e4e9dd8c66436891f44cd533861cc82d696a
Geovanni Perez <geovanni.perez@gmail.com>
2017-08-02 12:40:54 -0500

    Add JsonRequestException (#20)
    * Add JsonRequestException
    
    * Fixing issues from CR
    
    * Update JsonClient.cs
    
    * Add Unit Test

---
+++ src/Unosquare.Swan/Exceptions/JsonRequestException.cs
@@ -0,0 +1,34 @@
+﻿namespace Unosquare.Swan.Exceptions
+{
+    using System;
+
+    /// <summary>
+    /// Represents errors that occurs requesting a JSON file through HTTP
+    /// </summary>
+    /// <seealso cref="System.Exception" />
+#if NET452
+    [Serializable]
+#endif
+    public class JsonRequestException 
+        : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="JsonRequestException"/> class.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <param name="httpErrorCode">The HTTP error code.</param>
+        public JsonRequestException(string message, int httpErrorCode = 500)
+            : base(message)
+        {
+            HttpErrorCode = httpErrorCode;
+        }
+
+        /// <summary>
+        /// Gets the HTTP error code.
+        /// </summary>
+        /// <value>
+        /// The HTTP error code.
+        /// </value>
+        public int HttpErrorCode { get; }
+    }
+}
