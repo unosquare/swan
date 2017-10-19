e962693ec8037f8bf9c41331de6e91ca129e05db
Geovanni Perez <geovanni.perez@gmail.com>
2017-10-19 09:20:21 -0500

    Ldap exceptions (#35)
    * Use CheckResult for handle errors
    
    * Add test for CheckResults and ErrorConnection for LDAP
    
    * Modify tests
    
    * Change LdapTests
    
    * Correct observations
    
    * Improve tests
    
    * Correct obvservations
    
    * Renaming tests

--- test/Unosquare.Swan.Test/LdapTest.cs
+++ test/Unosquare.Swan.Test/LdapTest.cs
@@ -1,61 +1,146 @@
 ﻿using System.Threading.Tasks;
 using NUnit.Framework;
 using Unosquare.Swan.Networking.Ldap;
+using System;
+using System.Net.Sockets;
 
 namespace Unosquare.Swan.Test
 {
-    [TestFixture]
     public class LdapTest
     {
-        [Test]
-        public async Task ConnectTest()
+        private const string ldapServer = "ldap.forumsys.com";
+
+        [TestFixture]
+        public class Bind : LdapTest
         {
-            var cn = new LdapConnection();
-
-            await cn.Connect("ldap.forumsys.com", 389);
-            Assert.IsTrue(cn.Connected);
-        }
-
-        [Test]
-        public async Task BindTest()
-        {
-            var cn = new LdapConnection();
-
-            await cn.Connect("ldap.forumsys.com", 389);
-            await cn.Bind("uid=riemann,dc=example,dc=com", "password");
-        }
-
-        [Test]
-        public async Task SearchTest()
-        {
-            var cn = new LdapConnection();
-
-            await cn.Connect("ldap.forumsys.com", 389);
-            await cn.Bind("uid=riemann,dc=example,dc=com", "password");
-            var lsc = await cn.Search("ou=scientists,dc=example,dc=com", LdapConnection.ScopeSub);
-
-            if (lsc.HasMore())
+            [Test]
+            public async Task ValidCredentials_ReturnsTrue()
             {
-                var entry = lsc.Next();
-                var ldapAttributes = entry.GetAttributeSet();
-                var obj = ldapAttributes.GetAttribute("uniqueMember")?.StringValue;
-                Assert.IsTrue(obj != null);
+                var cn = new LdapConnection();
+                await cn.Connect(ldapServer, 389);
+                await cn.Bind("uid=riemann,dc=example,dc=com", "password");
+                Assert.IsNotNull(cn.AuthenticationDn);
+                cn.Disconnect();
             }
 
-            Assert.AreNotEqual(lsc.Count, 0);
-            Assert.IsTrue(lsc.HasMore());
+            [Test]
+            public void InvalidCredentials_ThrowsLdapException()
+            {
+                Assert.ThrowsAsync<LdapException>(async () =>
+                {
+                    var cn = new LdapConnection();
+                    await cn.Connect(ldapServer, 389);
+                    await cn.Bind("uid=riemann,dc=example", "password");
+                    cn.Disconnect();
+                });
+            }
         }
 
-        [Test]
-        public async Task Disconnect()
+        [TestFixture]
+        public class Connect : LdapTest
         {
-            var cn = new LdapConnection();
-            await cn.Connect("ldap.forumsys.com", 389);
-            await cn.Bind("uid=riemann,dc=example,dc=com", "password");
-            Assert.IsTrue(cn.Connected);
+            [Test]
+            public async Task ValidHost_ReturnsTrue()
+            {
+                var cn = new LdapConnection();
+                await cn.Connect(ldapServer, 389);
+                Assert.IsTrue(cn.Connected);
+                cn.Disconnect();
+            }
 
-            cn.Disconnect();
-            Assert.IsFalse(cn.Connected);
+            [Test]
+            public void InvalidHost_ThrowsSocketException()
+            {
+                Assert.ThrowsAsync<SocketException>(async () =>
+                {
+                    var cn = new LdapConnection();
+                    await cn.Connect("ldap.forumsys", 389);
+                    await cn.Bind("uid=riemann,dc=example,dc=com", "password");
+                    cn.Disconnect();
+                });
+            }
+
+            [Test]
+            public void InvalidPort_ThrowsSocketException()
+            {
+                Assert.ThrowsAsync<SocketException>(async () =>
+                {
+                    var cn = new LdapConnection();
+                    await cn.Connect("ldap.forumsys", 388);
+                    await cn.Bind("uid=riemann,dc=example,dc=com", "password");
+                    cn.Disconnect();
+                });
+            }
+
+            [Test]
+            public async Task Connected_ResetConnection()
+            {
+                var cn = new LdapConnection();
+                await cn.Connect(ldapServer, 389);
+                await cn.Bind("uid=riemann,dc=example,dc=com", "password");
+                Assert.IsTrue(cn.Connected);
+                cn.Disconnect();
+                Assert.IsFalse(cn.Connected);
+            }
+        }
+
+        [TestFixture]
+        public class Search : LdapTest
+        {
+            [Test]
+            public async Task MultipleSearchResults()
+            {
+                var cn = new LdapConnection();
+
+                await cn.Connect(ldapServer, 389);
+                await cn.Bind("uid=riemann,dc=example,dc=com", "password");
+                var lsc = await cn.Search("ou=scientists,dc=example,dc=com", LdapConnection.ScopeSub);
+
+                if (lsc.HasMore())
+                {
+                    var entry = lsc.Next();
+                    var ldapAttributes = entry.GetAttributeSet();
+                    var obj = ldapAttributes.GetAttribute("uniqueMember")?.StringValue;
+                    Assert.IsTrue(obj != null);
+                }
+
+                Assert.AreNotEqual(lsc.Count, 0);
+                Assert.IsTrue(lsc.HasMore());
+                cn.Disconnect();
+            }
+
+            [Test]
+            public async Task SingleSearchResult()
+            {
+                var cn = new LdapConnection();
+                await cn.Connect(ldapServer, 389);
+                await cn.Bind("uid=riemann,dc=example,dc=com", "password");
+                var lsc = await cn.Search("ou=scientists,dc=example,dc=com", LdapConnection.ScopeSub, "(uniqueMember=uid=tesla,dc=example,dc=com)");
+
+                if (lsc.HasMore())
+                {
+                    var entry = lsc.Next();
+                    var ldapAttributes = entry.GetAttributeSet();
+                    var obj = ldapAttributes.GetAttribute("uniqueMember")?.StringValue;
+                    Assert.IsTrue(obj != null);
+                }
+
+                Assert.AreNotEqual(lsc.Count, 0);
+                cn.Disconnect();
+            }
+
+            [Test]
+            public void UsingInvalidDN_ThrowsLdapException()
+            {
+                Assert.ThrowsAsync<LdapException>(async () =>
+                {
+                    var cn = new LdapConnection();
+                    await cn.Connect(ldapServer, 389);
+                    await cn.Bind("uid=riemann,dc=example,dc=com", "password");
+                    var lsc = await cn.Search("ou=scientists,dc=com", LdapConnection.ScopeSub);
+                    cn.Disconnect();
+                });
+            }
         }
     }
 }
