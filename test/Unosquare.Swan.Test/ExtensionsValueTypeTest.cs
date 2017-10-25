1c4d8db0472e447dcb9b3f3634e557bbdeb8c966
Armando Cifuentes <armando.cifuentes@unosquare.com>
2017-10-25 13:22:08 -0500

    Extensions.ValuesTypes.IsBetween => ValidData_ReturnsTrue test method added

--- test/Unosquare.Swan.Test/ExtensionsValueTypeTest.cs
+++ test/Unosquare.Swan.Test/ExtensionsValueTypeTest.cs
@@ -11,5 +11,12 @@ namespace Unosquare.Swan.Test
             Assert.AreEqual(3, 3.Clamp(1, 3));
             Assert.AreEqual(-1, -1.Clamp(1, 5));
         }
+
+        [Test]
+        public void IsBetween_ValidData_ReturnsTrue()
+        {
+            var aux = Extensions.IsBetween(5, 0, 7);
+            Assert.IsTrue(aux);
+        }
     }
 }
