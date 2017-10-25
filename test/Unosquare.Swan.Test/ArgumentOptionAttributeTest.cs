2c17ecffa255bcdf92e713dc78b70b43877a9e44
Armando Cifuentes <armando.cifuentes@unosquare.com>
2017-10-25 17:38:53 -0500

    ArgumentOptionAttribute.Constructor => NotInitialized_Valid test method added

---
+++ test/Unosquare.Swan.Test/ArgumentOptionAttributeTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Unosquare.Swan.Attributes;
+
+namespace Unosquare.Swan.Test.ArgumentOptionAttributeTests
+{
+    public abstract class ArgumentOptionAttributeTest
+    {
+        protected ArgumentOptionAttribute argOptAttr;
+    }
+
+    [TestFixture]
+    public class Constructor : ArgumentOptionAttributeTest
+    {
+        [Test]
+        public void NotInitialized_Valid()
+        {
+            argOptAttr = new ArgumentOptionAttribute();
+
+            Assert.IsNotNull(argOptAttr);
+        }
+    }
+}
