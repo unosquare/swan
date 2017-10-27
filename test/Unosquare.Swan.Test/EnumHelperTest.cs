502fd6d53fa1ec9ffa256621e1f6a93b565d67e7
buda1994 <hector.torres@unosquare.com>
2017-10-27 14:10:36 -0500

    Added a new enum in the EnumHelperTest class

--- test/Unosquare.Swan.Test/EnumHelperTest.cs
+++ test/Unosquare.Swan.Test/EnumHelperTest.cs
@@ -11,13 +11,24 @@ using Unosquare.Swan.Test.Mocks;
 
 namespace Unosquare.Swan.Test.EnumHelperTest
 {
+    public abstract class EnumHelperTest
+    {
+        public enum City
+        {
+            Stormwind = 1,
+            Suramar,
+            Orgrimmar,
+            Dalaran
+        }
+    }
+    
     [TestFixture]
-    public class GetItemsWithValue
+    public class GetItemsWithIndex
     {
         [Test]
         public void WithValidEnum_ReturnsTuple()
         {
-            var arc = EnumHelper.GetItemsWithValue<MyEnum>();
+            var arc = EnumHelper.GetItemsWithIndex<MyEnum>();
             
             Assert.AreEqual(arc[0].ToString(), "(0, One)");
             Assert.AreEqual(arc[1].ToString(), "(1, Two)");
@@ -26,17 +37,19 @@ namespace Unosquare.Swan.Test.EnumHelperTest
     }
 
     [TestFixture]
-    public class GetItemsWithIndex
+    public class GetItemsWithValue : EnumHelperTest
     {
         [Test]
         public void WithValidEnum_ReturnsTuple()
         {
-            var arc = EnumHelper.GetItemsWithIndex<MyEnum>();
+            var arc = EnumHelper.GetItemsWithValue<City>();
 
-            Assert.AreEqual(arc[0].ToString(), "(0, One)");
-            Assert.AreEqual(arc[1].ToString(), "(1, Two)");
-            Assert.AreEqual(arc[2].ToString(), "(2, Three)");
+            Assert.AreEqual(arc[0].ToString(), "(1, Stormwind)");
+            Assert.AreEqual(arc[1].ToString(), "(2, Suramar)");
+            Assert.AreEqual(arc[2].ToString(), "(3, Orgrimmar)");
+            Assert.AreEqual(arc[3].ToString(), "(4, Dalaran)");
         }
+
     }
 
 }
