aed16763ed16af28622c546ebdcaa584179f6b04
buda1994 <hector.torres@unosquare.com>
2017-10-27 13:23:52 -0500

    Added EnumHelperTest

---
+++ test/Unosquare.Swan.Test/EnumHelperTest.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Threading;
+using System.Threading.Tasks;
+using Unosquare.Swan.Components;
+using Unosquare.Swan.Test.Mocks;
+
+namespace Unosquare.Swan.Test.EnumHelperTest
+{
+    [TestFixture]
+    public class GetItemsWithValue
+    {
+        [Test]
+        public void WithValidEnum_ReturnsTuple()
+        {
+            var arc = EnumHelper.GetItemsWithValue<MyEnum>();
+            
+            Assert.AreEqual(arc[0].ToString(), "(0, One)");
+            Assert.AreEqual(arc[1].ToString(), "(1, Two)");
+            Assert.AreEqual(arc[2].ToString(), "(2, Three)");
+        }
+    }
+
+    [TestFixture]
+    public class GetItemsWithIndex
+    {
+        [Test]
+        public void WithValidEnum_ReturnsTuple()
+        {
+            var arc = EnumHelper.GetItemsWithIndex<MyEnum>();
+
+            Assert.AreEqual(arc[0].ToString(), "(0, One)");
+            Assert.AreEqual(arc[1].ToString(), "(1, Two)");
+            Assert.AreEqual(arc[2].ToString(), "(2, Three)");
+        }
+    }
+
+}
