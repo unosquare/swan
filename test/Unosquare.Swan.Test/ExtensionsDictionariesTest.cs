013c0516b2468045b43d8bbcfac50da0b1d1813f
Armando Cifuentes <armando.cifuentes@unosquare.com>
2017-10-30 11:45:59 -0600

    Extensions.Dictionaries.GetValueOrDefault => NullDictionary_ThrowsArgumentNullException test method added (class created. WIP)

---
+++ test/Unosquare.Swan.Test/ExtensionsDictionariesTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace Unosquare.Swan.Test.ExtensionsDictionariesTests
+{
+    public abstract class ExtensionsDictionariesTest
+    {
+        protected Dictionary<object, object> dict = new Dictionary<object, object>();
+    }
+
+    [TestFixture]
+    public class GetValueOrDefault : ExtensionsDictionariesTest
+    {
+        [Test]
+        public void NullDictionary_ThrowsArgumentNullException()
+        {
+            dict = null;
+
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                Extensions.GetValueOrDefault(dict, 1);
+            });
+        }
+    }
+}
