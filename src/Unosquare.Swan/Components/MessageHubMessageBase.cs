0836e778db2c9684a64b4fdca666450badd6b026
Geo Perez <geovanni.perez@gmail.com>
2017-11-06 16:48:15 -0600

    Improvements to MessageHub

---
+++ src/Unosquare.Swan/Components/MessageHubMessageBase.cs
@@ -0,0 +1,91 @@
+﻿namespace Unosquare.Swan.Components
+{
+    using System;
+
+    /// <summary>
+    /// Base class for messages that provides weak reference storage of the sender
+    /// </summary>
+    public abstract class MessageHubMessageBase : IMessageHubMessage
+    {
+        /// <summary>
+        /// Store a WeakReference to the sender just in case anyone is daft enough to
+        /// keep the message around and prevent the sender from being collected.
+        /// </summary>
+        private readonly WeakReference _sender;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MessageHubMessageBase"/> class.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <exception cref="System.ArgumentNullException">sender</exception>
+        protected MessageHubMessageBase(object sender)
+        {
+            if (sender == null)
+                throw new ArgumentNullException(nameof(sender));
+
+            _sender = new WeakReference(sender);
+        }
+
+        /// <summary>
+        /// The sender of the message, or null if not supported by the message implementation.
+        /// </summary>
+        public object Sender => _sender?.Target;
+    }
+
+    /// <summary>
+    /// Generic message with user specified content
+    /// </summary>
+    /// <typeparam name="TContent">Content type to store</typeparam>
+    public class MessageHubGenericMessage<TContent>
+        : MessageHubMessageBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MessageHubGenericMessage{TContent}"/> class.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="content">The content.</param>
+        public MessageHubGenericMessage(object sender, TContent content)
+            : base(sender)
+        {
+            Content = content;
+        }
+
+        /// <summary>
+        /// Contents of the message
+        /// </summary>
+        public TContent Content { get; protected set; }
+    }
+
+    /// <summary>
+    /// Basic "cancellable" generic message
+    /// </summary>
+    /// <typeparam name="TContent">Content type to store</typeparam>
+    public class MessageHubCancellableGenericMessage<TContent> 
+        : MessageHubMessageBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MessageHubCancellableGenericMessage{TContent}"/> class.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="content">The content.</param>
+        /// <param name="cancelAction">The cancel action.</param>
+        /// <exception cref="System.ArgumentNullException">cancelAction</exception>
+        public MessageHubCancellableGenericMessage(object sender, TContent content, Action cancelAction)
+            : base(sender)
+        {
+            Content = content;
+            Cancel = cancelAction ?? throw new ArgumentNullException(nameof(cancelAction));
+        }
+
+        /// <summary>
+        /// Cancel action
+        /// </summary>
+        public Action Cancel { get; protected set; }
+
+        /// <summary>
+        /// Contents of the message
+        /// </summary>
+        public TContent Content { get; protected set; }
+
+    }
+}
