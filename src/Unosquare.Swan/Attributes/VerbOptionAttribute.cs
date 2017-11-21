1f637ec85f5bc03c31189b697147991ff4171166
Geovanni Perez <geovanni.perez@gmail.com>
2017-11-21 14:04:07 -0600

    CLI Verbs Support (#45)
    * Add verb parsing
    
    * Add verb attribute
    
    * Fix verb parsing
    
    * Add unit tests
    
    * Update Unosquare.Swan.csproj

---
+++ src/Unosquare.Swan/Attributes/VerbOptionAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+ 
+namespace Unosquare.Swan.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public sealed class VerbOptionAttribute : Attribute
+    {
+        public VerbOptionAttribute(string name)
+        {
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+        }
+
+        public string Name { get; }
+    }
+}
---
+++ test/Unosquare.Swan.Test/Mocks/VerbOptionMock.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unosquare.Swan.Attributes;
+
+namespace Unosquare.Swan.Test.Mocks
+{
+    public class CliVerbs
+    {
+        [VerbOption("verb")]
+        public VerbOptions PushVerbOptions { get; set; }
+        [VerbOption("monitor")]
+        public MonitorOptions MonitorVerboptions { get; set; }
+    }
+
+    public class VerbOptions
+    {
+        [ArgumentOption('v', "verbose", HelpText = "Set verbose mode.")]
+        public bool Verbose { get; set; }
+
+        [ArgumentOption("color", DefaultValue = ConsoleColor.Red, HelpText = "Set background color.")]
+        public ConsoleColor BgColor { get; set; }
+
+        [ArgumentOption('n', HelpText = "Set user name.")]
+        public string Username { get; set; }
+    }
+
+    public class MonitorOptions
+    {
+        [ArgumentOption('v', "verbose", HelpText = "Set verbose mode.")]
+        public bool Verbose { get; set; }
+
+        [ArgumentOption("color", DefaultValue = ConsoleColor.Red, HelpText = "Set background color.")]
+        public ConsoleColor BgColor { get; set; }
+    }
+}
