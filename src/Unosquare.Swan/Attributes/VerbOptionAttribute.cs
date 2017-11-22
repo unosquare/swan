c3cb5ace9e41b50f41e481ae6206318aa6cf1699
Juan Alcala <juan.alcala@unosquare.com>
2017-11-22 13:33:53 -0600

    Add helpText to VerbOptionAttribute

--- src/Unosquare.Swan/Attributes/VerbOptionAttribute.cs
+++ src/Unosquare.Swan/Attributes/VerbOptionAttribute.cs
@@ -2,14 +2,35 @@
  
 namespace Unosquare.Swan.Attributes
 {
+    /// <summary>
+    /// Models a verb option
+    /// </summary>
     [AttributeUsage(AttributeTargets.Property)]
     public sealed class VerbOptionAttribute : Attribute
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="VerbOptionAttribute"/> class.
+        /// <param name="name">The name of the option verb.</param>
+        /// </summary>
         public VerbOptionAttribute(string name)
         {
             Name = name ?? throw new ArgumentNullException(nameof(name));
         }
 
+        /// <summary>
+        /// Gets the name of the verb option.
+        /// </summary>
+        /// <value>
+        /// Name.
+        /// </value>
         public string Name { get; }
+
+        /// <summary>
+        /// Gets or sets a short description of this command line verb. Usually a sentence summary.
+        /// </summary>
+        /// <value>
+        /// The help text.
+        /// </value>
+        public string HelpText { get; set; }
     }
 }
