e57d7b870188ca9658e54d4c83c48af7e8c945c3
Geovanni Perez <geovanni.perez@gmail.com>
2017-12-01 15:42:46 -0600

    Issue 49 CS project parser (#50)
    * Base classes
    
    * Add CsProjFile tests
    
    * Add constructor to CsProjFile
    
    * Versioning
    
    * Fix Test
    
    * Add CsProjFile constructor tests
    
    * Checking issue
    
    * Change LDAP test
    
    * Add Universal Time to Test
    
    * Remove buggy test case

---
+++ src/Unosquare.Swan/Components/CsProjFile.cs
@@ -0,0 +1,100 @@
+﻿namespace Unosquare.Swan.Components
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Xml.Linq;
+
+    /// <summary>
+    /// Represents a CsProjFile parser
+    /// Based on https://github.com/maartenba/dotnetcli-init
+    /// </summary>
+    /// <typeparam name="T">The type of <c>CsProjMetadataBase</c></typeparam>
+    /// <seealso cref="System.IDisposable" />
+    public class CsProjFile<T>
+        : IDisposable
+        where T : CsProjMetadataBase
+    {
+        private readonly Stream _stream;
+        private readonly bool _leaveOpen;
+        private readonly XDocument _xmlDocument;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CsProjFile{T}"/> class.
+        /// </summary>
+        /// <param name="filename">The filename.</param>
+        public CsProjFile(string filename = null)
+            : this(OpenFile(filename))
+        {
+            // placeholder
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CsProjFile{T}"/> class.
+        /// </summary>
+        /// <param name="stream">The stream.</param>
+        /// <param name="leaveOpen">if set to <c>true</c> [leave open].</param>
+        /// <exception cref="ArgumentException">Project file is not of the new .csproj type.</exception>
+        public CsProjFile(Stream stream, bool leaveOpen = false)
+        {
+            _stream = stream;
+            _leaveOpen = leaveOpen;
+
+            _xmlDocument = XDocument.Load(stream);
+
+            var projectElement = _xmlDocument.Descendants("Project").FirstOrDefault();
+            if (projectElement == null || projectElement.Attribute("Sdk")?.Value != "Microsoft.NET.Sdk")
+            {
+                throw new ArgumentException("Project file is not of the new .csproj type.");
+            }
+
+            Metadata = Activator.CreateInstance<T>();
+            Metadata.SetData(_xmlDocument);
+        }
+
+        /// <summary>
+        /// Gets the metadata.
+        /// </summary>
+        /// <value>
+        /// The nu get metadata.
+        /// </value>
+        public T Metadata { get; }
+
+        /// <summary>
+        /// Saves this instance.
+        /// </summary>
+        public void Save()
+        {
+            _stream.SetLength(0);
+            _stream.Position = 0;
+
+            _xmlDocument.Save(_stream);
+        }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            if (!_leaveOpen)
+            {
+                _stream?.Dispose();
+            }
+        }
+
+        private static FileStream OpenFile(string filename)
+        {
+            if (filename == null)
+            {
+                filename = Directory
+                    .EnumerateFiles(Directory.GetCurrentDirectory(), "*.csproj", SearchOption.TopDirectoryOnly)
+                    .FirstOrDefault();
+            }
+
+            if (string.IsNullOrWhiteSpace(filename))
+                throw new ArgumentNullException(nameof(filename));
+
+            return File.Open(filename, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+        }
+    }
+}
---
+++ src/Unosquare.Swan/Components/CsProjMetadataBase.cs
@@ -0,0 +1,79 @@
+﻿namespace Unosquare.Swan.Components
+{
+    using System.Linq;
+    using System.Xml.Linq;
+
+    /// <summary>
+    /// Represents a CsProj metadata abstract class
+    /// to use with <c>CsProjFile</c> parser.
+    /// </summary>
+    public abstract class CsProjMetadataBase
+    {
+        private XDocument _xmlDocument;
+
+        /// <summary>
+        /// Gets the package identifier.
+        /// </summary>
+        /// <value>
+        /// The package identifier.
+        /// </value>
+        public string PackageId => FindElement(nameof(PackageId))?.Value;
+
+        /// <summary>
+        /// Gets the name of the assembly.
+        /// </summary>
+        /// <value>
+        /// The name of the assembly.
+        /// </value>
+        public string AssemblyName => FindElement(nameof(AssemblyName))?.Value;
+
+        /// <summary>
+        /// Gets the target frameworks.
+        /// </summary>
+        /// <value>
+        /// The target frameworks.
+        /// </value>
+        public string TargetFrameworks => FindElement(nameof(TargetFrameworks))?.Value;
+
+        /// <summary>
+        /// Gets the target framework.
+        /// </summary>
+        /// <value>
+        /// The target framework.
+        /// </value>
+        public string TargetFramework => FindElement(nameof(TargetFramework))?.Value;
+
+        /// <summary>
+        /// Gets the version.
+        /// </summary>
+        /// <value>
+        /// The version.
+        /// </value>
+        public string Version => FindElement(nameof(Version))?.Value;
+
+        /// <summary>
+        /// Parses the cs proj tags.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        public abstract void ParseCsProjTags(ref string[] args);
+
+        /// <summary>
+        /// Sets the data.
+        /// </summary>
+        /// <param name="xmlDocument">The XML document.</param>
+        public void SetData(XDocument xmlDocument)
+        {
+            _xmlDocument = xmlDocument;
+        }
+
+        /// <summary>
+        /// Finds the element.
+        /// </summary>
+        /// <param name="elementName">Name of the element.</param>
+        /// <returns>A XElement.</returns>
+        protected XElement FindElement(string elementName)
+        {
+            return _xmlDocument.Descendants(elementName).FirstOrDefault();
+        }
+    }
+}
