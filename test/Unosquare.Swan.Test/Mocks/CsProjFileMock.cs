e29e0d66a3ec293c6b497bf6f4602d40db890742
Juan Alcala <juan.alcala@unosquare.com>
2017-12-01 12:44:31 -0600

    Add CsProjFile tests

---
+++ test/Unosquare.Swan.Test/Mocks/CsProjFileMock.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unosquare.Swan.Components;
+
+namespace Unosquare.Swan.Test.Mocks
+{
+    public class CsMetadataMock : CsProjMetadataBase
+    {
+        public string Copyright => FindElement(nameof(Copyright))?.Value;
+
+        public string NonExistentProp => FindElement(nameof(NonExistentProp))?.Value;
+
+        public override void ParseCsProjTags(ref string[] args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public abstract class CsAbstractMetadataMock : CsProjMetadataBase
+    {
+        public override void ParseCsProjTags(ref string[] args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
