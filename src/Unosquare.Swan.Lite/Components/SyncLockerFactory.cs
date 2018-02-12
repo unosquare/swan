c74134b10cae2e321967974422e82d7540169249
Juan Alcala <juan.alcala@unosquare.com>
2018-02-12 14:13:22 -0600

    Add FFME primitives

---
+++ src/Unosquare.Swan.Lite/Components/SyncLockerFactory.cs
@@ -0,0 +1,240 @@
+﻿namespace Unosquare.Swan.Lite.Components
+{
+    using System;
+    using System.Threading;
+    using Unosquare.Swan.Lite.Abstractions;
+
+    /// <summary>
+    /// Provides factory methods to create synchronized reader-writer locks
+    /// that support a generalized locking and releasing api and syntax.
+    /// </summary>
+    public static class SyncLockerFactory
+    {
+#region Enums and Interfaces
+
+        /// <summary>
+        /// Enumerates the locking operations
+        /// </summary>
+        private enum LockHolderType
+        {
+            Read,
+            Write,
+        }
+
+        /// <summary>
+        /// Defines methods for releasing locks
+        /// </summary>
+        private interface ISyncReleasable
+        {
+            /// <summary>
+            /// Releases the writer lock.
+            /// </summary>
+            void ReleaseWriterLock();
+
+            /// <summary>
+            /// Releases the reader lock.
+            /// </summary>
+            void ReleaseReaderLock();
+        }
+
+#endregion
+
+#region Factory Methods
+
+        /// <summary>
+        /// Creates a reader-writer lock backed by a standard ReaderWriterLock
+        /// </summary>
+        /// <returns>The synchronized locker</returns>
+        public static ISyncLocker Create() => new SyncLocker();
+
+        /// <summary>
+        /// Creates a reader-writer lock backed by a ReaderWriterLockSlim
+        /// </summary>
+        /// <returns>The synchronized locker</returns>
+        public static ISyncLocker CreateSlim() => new SyncLockerSlim();
+
+        /// <summary>
+        /// Creates a reader-writer lock.
+        /// </summary>
+        /// <param name="useSlim">if set to <c>true</c> it uses the Slim version of a reader-writer lock.</param>
+        /// <returns>The Sync Locker</returns>
+        public static ISyncLocker Create(bool useSlim) => useSlim ? CreateSlim() : Create();
+
+#endregion
+
+#region Private Classes
+
+        /// <summary>
+        /// The lock releaser. Calling the dispose method releases the lock entered by the parent SyncLocker.
+        /// </summary>
+        /// <seealso cref="System.IDisposable" />
+        private sealed class SyncLockReleaser : IDisposable
+        {
+            private bool IsDisposed = false;
+            private ISyncReleasable Parent = null;
+            private LockHolderType Operation;
+
+            /// <summary>
+            /// Initializes a new instance of the <see cref="SyncLockReleaser"/> class.
+            /// </summary>
+            /// <param name="parent">The parent.</param>
+            /// <param name="operation">The operation.</param>
+            public SyncLockReleaser(ISyncReleasable parent, LockHolderType operation)
+            {
+                Parent = parent;
+                Operation = operation;
+            }
+
+            /// <summary>
+            /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+            /// </summary>
+            public void Dispose() => Dispose(true);
+
+            /// <summary>
+            /// Releases unmanaged and - optionally - managed resources.
+            /// </summary>
+            /// <param name="alsoManaged"><c>true</c> to release both managed and unmanaged resources; <c>false</c> to release only unmanaged resources.</param>
+            private void Dispose(bool alsoManaged)
+            {
+                if (IsDisposed) return;
+                IsDisposed = true;
+
+                if (Operation == LockHolderType.Read)
+                    Parent.ReleaseReaderLock();
+                else
+                    Parent.ReleaseWriterLock();
+            }
+        }
+
+        /// <summary>
+        /// The Sync Locker backed by a ReaderWriterLock
+        /// </summary>
+        /// <seealso cref="ISyncLocker" />
+        /// <seealso cref="ISyncReleasable" />
+        private sealed class SyncLocker : ISyncLocker, ISyncReleasable
+        {
+            private bool IsDisposed = false;
+            private ReaderWriterLock Locker = new ReaderWriterLock();
+
+            /// <summary>
+            /// Acquires a reader lock.
+            /// The lock is released when the returned locking object is disposed.
+            /// </summary>
+            /// <returns>
+            /// A disposable locking object.
+            /// </returns>
+            public IDisposable AcquireReaderLock()
+            {
+                Locker?.AcquireReaderLock(Timeout.Infinite);
+                return new SyncLockReleaser(this, LockHolderType.Read);
+            }
+
+            /// <summary>
+            /// Acquires a writer lock.
+            /// The lock is released when the returned locking object is disposed.
+            /// </summary>
+            /// <returns>
+            /// A disposable locking object.
+            /// </returns>
+            public IDisposable AcquireWriterLock()
+            {
+                Locker?.AcquireWriterLock(Timeout.Infinite);
+                return new SyncLockReleaser(this, LockHolderType.Write);
+            }
+
+            /// <summary>
+            /// Releases the writer lock.
+            /// </summary>
+            public void ReleaseWriterLock() => Locker?.ReleaseWriterLock();
+
+            /// <summary>
+            /// Releases the reader lock.
+            /// </summary>
+            public void ReleaseReaderLock() => Locker?.ReleaseReaderLock();
+
+            /// <summary>
+            /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+            /// </summary>
+            public void Dispose() => Dispose(true);
+
+            /// <summary>
+            /// Releases unmanaged and - optionally - managed resources.
+            /// </summary>
+            /// <param name="alsoManaged"><c>true</c> to release both managed and unmanaged resources; <c>false</c> to release only unmanaged resources.</param>
+            private void Dispose(bool alsoManaged)
+            {
+                if (IsDisposed) return;
+                IsDisposed = true;
+                Locker?.ReleaseLock();
+                Locker = null;
+            }
+        }
+
+        /// <summary>
+        /// The Sync Locker backed by ReaderWriterLockSlim
+        /// </summary>
+        /// <seealso cref="ISyncLocker" />
+        /// <seealso cref="ISyncReleasable" />
+        private sealed class SyncLockerSlim : ISyncLocker, ISyncReleasable
+        {
+            private bool IsDisposed = false;
+            private ReaderWriterLockSlim Locker
+                = new ReaderWriterLockSlim(LockRecursionPolicy.SupportsRecursion);
+
+            /// <summary>
+            /// Acquires a reader lock.
+            /// The lock is released when the returned locking object is disposed.
+            /// </summary>
+            /// <returns>
+            /// A disposable locking object.
+            /// </returns>
+            public IDisposable AcquireReaderLock()
+            {
+                Locker?.EnterReadLock();
+                return new SyncLockReleaser(this, LockHolderType.Read);
+            }
+
+            /// <summary>
+            /// Acquires a writer lock.
+            /// The lock is released when the returned locking object is disposed.
+            /// </summary>
+            /// <returns>
+            /// A disposable locking object.
+            /// </returns>
+            public IDisposable AcquireWriterLock()
+            {
+                Locker?.EnterWriteLock();
+                return new SyncLockReleaser(this, LockHolderType.Write);
+            }
+
+            /// <summary>
+            /// Releases the writer lock.
+            /// </summary>
+            public void ReleaseWriterLock() => Locker?.ExitWriteLock();
+
+            /// <summary>
+            /// Releases the reader lock.
+            /// </summary>
+            public void ReleaseReaderLock() => Locker?.ExitReadLock();
+
+            /// <summary>
+            /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+            /// </summary>
+            public void Dispose() => Dispose(true);
+
+            /// <summary>
+            /// Releases unmanaged and - optionally - managed resources.
+            /// </summary>
+            /// <param name="alsoManaged"><c>true</c> to release both managed and unmanaged resources; <c>false</c> to release only unmanaged resources.</param>
+            private void Dispose(bool alsoManaged)
+            {
+                if (IsDisposed) return;
+                IsDisposed = true;
+                Locker?.Dispose();
+                Locker = null;
+            }
+        }
+
+#endregion
+    }
+}
---
+++ src/Unosquare.Swan/Components/RealtimeClock.cs
@@ -0,0 +1,152 @@
+﻿namespace Unosquare.Swan.Components
+{
+    using System;
+    using System.Diagnostics;
+    using Unosquare.Swan.Lite.Abstractions;
+    using Unosquare.Swan.Lite.Components;
+
+    /// <summary>
+    /// A time measurement artifact.
+    /// </summary>
+    internal sealed class RealTimeClock : IDisposable
+    {
+        private readonly Stopwatch Chrono = new Stopwatch();
+        private ISyncLocker Locker = SyncLockerFactory.Create(useSlim: true);
+        private long OffsetTicks = 0;
+        private double m_SpeedRatio = 1.0d;
+        private bool IsDisposed = false;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RealTimeClock"/> class.
+        /// The clock starts paused and at the 0 position.
+        /// </summary>
+        public RealTimeClock()
+        {
+            Reset();
+        }
+
+        /// <summary>
+        /// Gets or sets the clock position.
+        /// </summary>
+        public TimeSpan Position
+        {
+            get
+            {
+                using (Locker.AcquireReaderLock())
+                {
+                    return TimeSpan.FromTicks(
+                        OffsetTicks + Convert.ToInt64(Chrono.Elapsed.Ticks * SpeedRatio));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether the clock is running.
+        /// </summary>
+        public bool IsRunning
+        {
+            get
+            {
+                using (Locker.AcquireReaderLock())
+                {
+                    return Chrono.IsRunning;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the speed ratio at which the clock runs.
+        /// </summary>
+        public double SpeedRatio
+        {
+            get
+            {
+                using (Locker.AcquireReaderLock())
+                {
+                    return m_SpeedRatio;
+                }
+            }
+            set
+            {
+                using (Locker.AcquireWriterLock())
+                {
+                    if (value < 0d) value = 0d;
+
+                    // Capture the initial position se we set it even after the speedratio has changed
+                    // this ensures a smooth position transition
+                    var initialPosition = Position;
+                    m_SpeedRatio = value;
+                    Update(initialPosition);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Sets a new position value atomically
+        /// </summary>
+        /// <param name="value">The new value that the position porperty will hold.</param>
+        public void Update(TimeSpan value)
+        {
+            using (Locker.AcquireWriterLock())
+            {
+                var resume = Chrono.IsRunning;
+                Chrono.Reset();
+                OffsetTicks = value.Ticks;
+                if (resume) Chrono.Start();
+            }
+        }
+
+        /// <summary>
+        /// Starts or resumes the clock.
+        /// </summary>
+        public void Play()
+        {
+            using (Locker.AcquireWriterLock())
+            {
+                if (Chrono.IsRunning) return;
+                Chrono.Start();
+            }
+        }
+
+        /// <summary>
+        /// Pauses the clock.
+        /// </summary>
+        public void Pause()
+        {
+            using (Locker.AcquireWriterLock())
+            {
+                Chrono.Stop();
+            }
+        }
+
+        /// <summary>
+        /// Sets the clock position to 0 and stops it.
+        /// The speed ratio is not modified.
+        /// </summary>
+        public void Reset()
+        {
+            using (Locker.AcquireWriterLock())
+            {
+                OffsetTicks = 0;
+                Chrono.Reset();
+            }
+        }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose() => Dispose(true);
+
+        /// <summary>
+        /// Releases unmanaged and - optionally - managed resources.
+        /// </summary>
+        /// <param name="alsoManaged"><c>true</c> to release both managed and unmanaged resources; <c>false</c> to release only unmanaged resources.</param>
+        private void Dispose(bool alsoManaged)
+        {
+            if (IsDisposed) return;
+            IsDisposed = true;
+            Locker?.Dispose();
+            Locker = null;
+        }
+    }
+}
