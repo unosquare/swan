1fcd5918d1bdd8477a1979375b86e974d48ab067
Juan Alcala <juan.alcala@unosquare.com>
2018-02-14 16:07:58 -0600

    Fix AtomicLongTest

--- test/Unosquare.Swan.Test/AtomicLongTest.cs
+++ test/Unosquare.Swan.Test/AtomicLongTest.cs
@@ -1,34 +1,33 @@
 ﻿namespace Unosquare.Swan.Test
 {
     using NUnit.Framework;
+    using System;
     using System.Threading;
+    using System.Threading.Tasks;
 
     [TestFixture]
-    class AtomicLongTest
+    public class AtomicLongTest
     {
-        private long count = 0;
         private AtomicLong atomic = new AtomicLong(0);
 
         [Test]
         public void Atomicity()
         {
-            var t1 = new Thread(SumTask);
-            var t2 = new Thread(SumTask);
-            var t3 = new Thread(SumTask);
+            var t1 = new Task(new Action(SumTask));
+            var t2 = new Task(new Action(SumTask));
+            var t3 = new Task(new Action(SumTask));
 
             t1.Start();
             t2.Start();
             t3.Start();
 
-            t1.Join();
-            t2.Join();
-            t3.Join();
-            Assert.That(atomic.Value, Is.EqualTo(30000));
+            Task.WaitAll(t1, t2, t3);
+            Assert.That(atomic.Value, Is.EqualTo(3000));
         }
 
         private void SumTask()
         {
-            for (int x = 0; x < 10000; x++)
+            for (int x = 0; x < 1000; x++)
             {
                 atomic.Value++;
             }
