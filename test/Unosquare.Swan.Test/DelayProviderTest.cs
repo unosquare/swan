b04e121ff548780ae17d2aed6fe5b572caaa77a3
Juan Alcala <juan.alcala@unosquare.com>
2018-02-14 14:44:20 -0600

    Add DelayProvider tests

---
+++ test/Unosquare.Swan.Test/DelayProviderTest.cs
@@ -0,0 +1,50 @@
+﻿namespace Unosquare.Swan.Test
+{
+    using NUnit.Framework;
+    using Unosquare.Swan.Components;
+
+    [TestFixture]
+    public class DelayProviderThreadSleep
+    {
+        [Test]
+        public void WaitOne_TakesCertainTime()
+        {
+            using (var delay = new DelayProvider(DelayProvider.DelayStrategy.ThreadSleep))
+            {
+                var time = delay.WaitOne();
+                var mil = time.Milliseconds;
+                Assert.Greater(mil, 1);
+            }               
+        }
+    }
+
+    [TestFixture]
+    public class DelayProviderTaskWait
+    {
+        [Test]
+        public void WaitOne_TakesCertainTime()
+        {
+            using (var delay = new DelayProvider(DelayProvider.DelayStrategy.TaskDelay))
+            {
+                var time = delay.WaitOne();
+                var mil = time.Milliseconds;
+                Assert.Greater(mil, 1);
+            }
+        }
+    }
+
+    [TestFixture]
+    public class DelayProviderThreadPool
+    {
+        [Test]
+        public void WaitOne_TakesCertainTime()
+        {
+            using (var delay = new DelayProvider(DelayProvider.DelayStrategy.ThreadPool))
+            {
+                var time = delay.WaitOne();
+                var mil = time.Milliseconds;
+                Assert.Greater(mil, 1);
+            }
+        }
+    }
+}
