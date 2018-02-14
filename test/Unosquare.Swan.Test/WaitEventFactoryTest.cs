78daa0a320c36fd85673f0cd17fb1cb1fe20b53f
Juan Alcala <juan.alcala@unosquare.com>
2018-02-14 15:10:00 -0600

    Refactor test using test cases

--- test/Unosquare.Swan.Test/WaitEventFactoryTest.cs
+++ test/Unosquare.Swan.Test/WaitEventFactoryTest.cs
@@ -7,9 +7,11 @@
     public class WaitEvent
     {
         [Test]
-        public void NormalCycle()
+        [TestCase(true,true)]
+        [TestCase(true,false)]
+        public void NormalCycle(bool initialValue, bool useSlim)
         {
-            var wait = WaitEventFactory.Create(true);
+            var wait = WaitEventFactory.Create(initialValue, useSlim);
 
             Assert.IsTrue(wait.IsCompleted);
 
@@ -28,39 +30,6 @@
         public void IfDisposed_IsInProgressEqualsFalse()
         {
             var wait = WaitEventFactory.Create(true);
-            wait.Begin();
-            wait.Dispose();
-            wait.Begin();
-
-            Assert.IsFalse(wait.IsInProgress);
-            Assert.IsFalse(wait.IsValid);
-        }
-    }
-
-    [TestFixture]
-    public class WaitEventSllim
-    {
-        [Test]
-        public void NormalCycle()
-        {
-            var wait = WaitEventFactory.Create(true, true);
-            Assert.IsTrue(wait.IsCompleted);
-
-            wait.Begin();
-
-            Assert.IsFalse(wait.IsCompleted);
-            Assert.IsTrue(wait.IsInProgress);
-
-            wait.Complete();
-
-            Assert.IsTrue(wait.IsCompleted);
-            Assert.IsFalse(wait.IsInProgress);
-        }
-
-        [Test]
-        public void IfDisposed_IsInProgressEqualsFalse()
-        {
-            var wait = WaitEventFactory.Create(true, true);
             wait.Begin();
             wait.Dispose();
             wait.Begin();
