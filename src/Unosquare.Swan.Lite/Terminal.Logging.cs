f9ed8e1f5c749f1c8b044cf6033b0b0ab702a8cd
bufferUnderrun <github@pakkun.fr>
2018-02-15 17:09:36 +0100

    add new Fatal() methods to TerminalLoggin class

--- src/Unosquare.Swan.Lite/Terminal.Logging.cs
+++ src/Unosquare.Swan.Lite/Terminal.Logging.cs
@@ -20,7 +20,7 @@
 
         /// <summary>
         /// Occurs asynchronously, whenever a logging message is received by the terminal.
-        /// Only called when Terminal writes data via Info, Error, Trace, Warn, Debug methods, regardless of whether or not
+        /// Only called when Terminal writes data via Info, Error, Trace, Warn, Fatal, Debug methods, regardless of whether or not
         /// the console is present. Subscribe to this event to pass data on to your own logger.
         /// </summary>
         public static event EventHandler<LogMessageReceivedEventArgs> OnLogMessageReceived;
@@ -29,7 +29,7 @@
         /// Occurs synchronously (so handle quickly), whenever a logging message is about to be enqueued to the
         /// console output. Setting the CancelOutput to true in the event arguments prevents the
         /// logging message to be written out to the console.
-        /// Message filtering only works with logging methods such as Trace, Debug, Info, Warn, Error and Dump
+        /// Message filtering only works with logging methods such as Trace, Debug, Info, Warn, Fatal, Error and Dump
         /// Standard Write methods do not get filtering capabilities.
         /// </summary>
         public static event EventHandler<LogMessageDisplayingEventArgs> OnLogMessageDisplaying;
@@ -87,6 +87,10 @@
                     case LogMessageType.Warning:
                         color = Settings.WarnColor;
                         prefix = Settings.WarnPrefix;
+                        break;
+                    case LogMessageType.Fatal:
+                        color = Settings.FatalColor;
+                        prefix = Settings.FatalPrefix;
                         break;
                     default:
                         color = Settings.DefaultColor;
@@ -384,6 +388,70 @@
             [CallerLineNumber] int callerLineNumber = 0)
         {
             LogMessage(LogMessageType.Warning, message, source, extendedData, callerMemberName, callerFilePath, callerLineNumber);
+        }
+
+        #endregion
+
+        #region Fatal
+
+        /// <summary>
+        /// Logs a warning message to the console
+        /// </summary>
+        /// <param name="message">The text.</param>
+        /// <param name="source">The source.</param>
+        /// <param name="extendedData">The extended data.</param>
+        /// <param name="callerMemberName">Name of the caller member. This is automatically populated.</param>
+        /// <param name="callerFilePath">The caller file path. This is automatically populated.</param>
+        /// <param name="callerLineNumber">The caller line number. This is automatically populated.</param>
+        public static void Fatal(
+            this string message, 
+            string source = null, 
+            object extendedData = null,
+            [CallerMemberName] string callerMemberName = "",
+            [CallerFilePath] string callerFilePath = "",
+            [CallerLineNumber] int callerLineNumber = 0)
+        {
+            LogMessage(LogMessageType.Fatal, message, source, extendedData, callerMemberName, callerFilePath, callerLineNumber);
+        }
+
+        /// <summary>
+        /// Logs a warning message to the console
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <param name="source">The source.</param>
+        /// <param name="extendedData">The extended data.</param>
+        /// <param name="callerMemberName">Name of the caller member.</param>
+        /// <param name="callerFilePath">The caller file path.</param>
+        /// <param name="callerLineNumber">The caller line number.</param>
+        public static void Fatal(
+            this string message, 
+            Type source, 
+            object extendedData = null,
+            [CallerMemberName] string callerMemberName = "",
+            [CallerFilePath] string callerFilePath = "",
+            [CallerLineNumber] int callerLineNumber = 0)
+        {
+            LogMessage(LogMessageType.Fatal, message, source?.FullName, extendedData, callerMemberName, callerFilePath, callerLineNumber);
+        }
+
+        /// <summary>
+        /// Logs a warning message to the console
+        /// </summary>
+        /// <param name="extendedData">The extended data.</param>
+        /// <param name="source">The source.</param>
+        /// <param name="message">The message.</param>
+        /// <param name="callerMemberName">Name of the caller member. This is automatically populated.</param>
+        /// <param name="callerFilePath">The caller file path. This is automatically populated.</param>
+        /// <param name="callerLineNumber">The caller line number. This is automatically populated.</param>
+        public static void Fatal(
+            this Exception extendedData, 
+            string source, 
+            string message,
+            [CallerMemberName] string callerMemberName = "",
+            [CallerFilePath] string callerFilePath = "",
+            [CallerLineNumber] int callerLineNumber = 0)
+        {
+            LogMessage(LogMessageType.Fatal, message, source, extendedData, callerMemberName, callerFilePath, callerLineNumber);
         }
 
         #endregion
