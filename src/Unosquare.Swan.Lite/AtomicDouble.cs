14615fb0e629c16b30025b79db4cdf597e0b6a55
Juan Pablo Alcalá <32104554+jpalcala@users.noreply.github.com>
2018-02-20 16:46:39 -0600

    Update atomic types (#85)
    * Update atomic types
    
    * Use expression body definitions
    
    * Refactor code

--- src/Unosquare.Swan.Lite/AtomicDouble.cs
+++ src/Unosquare.Swan.Lite/AtomicDouble.cs
@@ -1,56 +1,31 @@
 ﻿namespace Unosquare.Swan
 {
     using System;
-    using System.Threading;
+    using Unosquare.Swan.Lite.Abstractions;
 
     /// <summary>
     /// Fast, atomic double combining interlocked to write value and volatile to read values
     /// Idea taken from Memory model and .NET operations in article:
     /// http://igoro.com/archive/volatile-keyword-in-c-memory-model-explained/
     /// </summary>
-    public sealed class AtomicDouble
+    public sealed class AtomicDouble : AtomicTypeBase<double>
     {
-        private long _value;
-
         /// <summary>
         /// Initializes a new instance of the <see cref="AtomicDouble"/> class.
         /// </summary>
-        /// <param name="initialValue">The initial value.</param>
-        public AtomicDouble(double initialValue = default) => Value = initialValue;
-
-        /// <summary>
-        /// Gets or sets the latest value written by any of the processors in the machine
-        /// </summary>
-        public double Value
+        /// <param name="initialValue">if set to <c>true</c> [initial value].</param>
+        public AtomicDouble(double initialValue = default)
+            : base(BitConverter.DoubleToInt64Bits(initialValue))
         {
-            get => BitConverter.Int64BitsToDouble(Interlocked.Read(ref _value));
-            set => Interlocked.Exchange(ref _value, BitConverter.DoubleToInt64Bits(value));
+            // placeholder
         }
 
-        /// <summary>
-        /// Implements the operator ++.
-        /// </summary>
-        /// <param name="instance">The instance.</param>
-        /// <returns>
-        /// The result of the operator.
-        /// </returns>
-        public static AtomicDouble operator ++(AtomicDouble instance)
-        {
-            instance.Value++;
-            return instance;
-        }
+        /// <inheritdoc/>
+        protected override double FromLong(long backingValue) => 
+            BitConverter.Int64BitsToDouble(backingValue);
 
-        /// <summary>
-        /// Implements the operator --.
-        /// </summary>
-        /// <param name="instance">The instance.</param>
-        /// <returns>
-        /// The result of the operator.
-        /// </returns>
-        public static AtomicDouble operator --(AtomicDouble instance)
-        {
-            instance.Value--;
-            return instance;
-        }
+        /// <inheritdoc/>
+        protected override long ToLong(double value) => 
+            BitConverter.DoubleToInt64Bits(value);
     }
 }
--- src/Unosquare.Swan.Lite/AtomicLong.cs
+++ src/Unosquare.Swan.Lite/AtomicLong.cs
@@ -1,59 +1,40 @@
 ﻿namespace Unosquare.Swan
 {
-    using System.Threading;
+    using Unosquare.Swan.Lite.Abstractions;
 
     /// <summary>
     /// Fast, atomioc long combining interlocked to write value and volatile to read values
     /// Idea taken from Memory model and .NET operations in article:
     /// http://igoro.com/archive/volatile-keyword-in-c-memory-model-explained/
     /// </summary>
-    public sealed class AtomicLong
+    public sealed class AtomicLong : AtomicTypeBase<long>
     {
-        private long _value;
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AtomicLong"/> class.
+        /// </summary>
+        /// <param name="initialValue">if set to <c>true</c> [initial value].</param>
+        public AtomicLong(long initialValue = default)
+            : base(initialValue)
+        {
+            // placeholder
+        }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="AtomicLong" /> class.
+        /// Converts froma long value to the target type.
+        /// </summary>
+        /// <param name="backingValue">The backing value.</param>
+        /// <returns>
+        /// The value converted form a long value
+        /// </returns>
+        protected override long FromLong(long backingValue) => backingValue;
+
+        /// <summary>
+        /// Converts from the target type to a long value
         /// </summary>
         /// <param name="value">The value.</param>
-        public AtomicLong(long value = default) => Value = value;
-
-        /// <summary>
-        /// Gets or sets the latest value written by any of the processors in the machine
-        /// </summary>
-        public long Value
-        {
-            get => Interlocked.Read(ref _value);
-            set => Interlocked.Exchange(ref _value, value);
-        }
-
-        /// <summary>
-        /// Implements the operator ++.
-        /// </summary>
-        /// <param name="instance">The instance.</param>
         /// <returns>
-        /// The result of the operator.
+        /// The value converted to a long value
         /// </returns>
-        public static AtomicLong operator ++(AtomicLong instance)
-        {
-            instance.IncreaseOne();
-            return instance;
-        }
-
-        /// <summary>
-        /// Implements the operator --.
-        /// </summary>
-        /// <param name="instance">The instance.</param>
-        /// <returns>
-        /// The result of the operator.
-        /// </returns>
-        public static AtomicLong operator --(AtomicLong instance)
-        {
-            instance.DecreaseOne();
-            return instance;
-        }
-
-        internal void IncreaseOne() => Interlocked.Increment(ref _value);
-
-        internal void DecreaseOne() => Interlocked.Decrement(ref _value);
+        protected override long ToLong(long value) => value;
     }
 }
