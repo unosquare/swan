08cd793aa784c0d7ddb95ea60f1e467a4e2a2750
Geo Perez <geovanni.perez@gmail.com>
2018-02-28 16:35:38 -0600

    Sample Retrieve

--- src/Unosquare.Swan.Lite/Reflection/AttributeCache.cs
+++ src/Unosquare.Swan.Lite/Reflection/AttributeCache.cs
@@ -1,11 +1,20 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Linq;
+using System.Reflection;
 
 namespace Unosquare.Swan.Reflection
 {
     using Components;
 
-    public class AttributeCache : CacheRepository<PropertyInfo, object>
+    public class AttributeCache : CacheRepository<MemberInfo, object>
     {
-        // TODO: Complete
+        public object[] Retrieve<T>(MemberInfo member, bool inherit = false)
+            where T : Attribute
+        {
+            if (member == null)
+                throw new ArgumentNullException(nameof(member));
+
+            return Retrieve(member, () => member.GetCustomAttributes<T>(inherit));
+        }
     }
 }
