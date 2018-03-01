920c4af75127f33e7f87947da006278c9c02fe3b
Pablo Alcala <juan.alcala@unosquare.com>
2018-03-01 12:16:46 -0600

    Add method to retrieve dictionary of properties and their attributes

--- src/Unosquare.Swan.Lite/Reflection/AttributeCache.cs
+++ src/Unosquare.Swan.Lite/Reflection/AttributeCache.cs
@@ -1,13 +1,18 @@
-﻿using System;
-using System.Linq;
-using System.Reflection;
-
-namespace Unosquare.Swan.Reflection
+﻿namespace Unosquare.Swan.Reflection
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
     using Components;
 
     public class AttributeCache : CacheRepository<MemberInfo, object>
     {
+        public PropertyTypeCache PropertyTypeCache { get; }
+        public AttributeCache(PropertyTypeCache propertyCache = null){
+            PropertyTypeCache = propertyCache ?? Runtime.PropertyTypeCache.Value;
+            }
+
         public object[] Retrieve<T>(MemberInfo member, bool inherit = false)
             where T : Attribute
         {
@@ -15,6 +20,16 @@ namespace Unosquare.Swan.Reflection
                 throw new ArgumentNullException(nameof(member));
 
             return Retrieve(member, () => member.GetCustomAttributes<T>(inherit));
+        }
+
+        public Dictionary<PropertyInfo, object[]> Retrieve<T>(Type type, bool inherit = false)
+            where T : Attribute
+        {
+            if (type == null)
+                throw new ArgumentNullException(nameof(type));
+
+            return PropertyTypeCache.Retrieve(type, PropertyTypeCache.GetAllPublicPropertiesFunc(type))
+                .ToDictionary(x => x as PropertyInfo, x => Retrieve<T>(x, inherit));
         }
     }
 }
