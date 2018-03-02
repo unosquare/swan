e9b4f5d1aaed02ab711b892a0b18e72e2f9fd331
Geo Perez <geovanni.perez@gmail.com>
2018-03-02 10:32:32 -0600

    Better API (I hate you Alcala)

--- src/Unosquare.Swan.Lite/Components/ObjectValidator.cs
+++ src/Unosquare.Swan.Lite/Components/ObjectValidator.cs
@@ -1,6 +1,7 @@
 ﻿namespace Unosquare.Swan.Components
 {
     using System;
+    using System.Linq;
     using System.Collections.Generic;
     using Attributes;
 #if NETSTANDARD1_3 || UWP
@@ -12,7 +13,7 @@
     /// </summary>
     public class ObjectValidator
     {
-        private readonly Dictionary<Type, List<Tuple<Delegate,string>>> _predicates =
+        private readonly Dictionary<Type, List<Tuple<Delegate, string>>> _predicates =
             new Dictionary<Type, List<Tuple<Delegate, string>>>();
 
         /// <summary>
@@ -21,75 +22,39 @@
         /// <typeparam name="T">The type of the object</typeparam>
         /// <param name="obj">The object</param>
         /// <returns>A bool indicating if it is a valid object</returns>
-        public List<string> Validate<T>(T obj)
+        public ObjectValidationResult Validate<T>(T obj)
         {
             if (Equals(obj, null))
                 throw new ArgumentNullException(nameof(obj));
 
-            var errorList = new List<string>();
-
-            if (_predicates.ContainsKey(typeof(T)))
-            {
-                foreach (var validation in _predicates[typeof(T)])
-                {
-                    if (!(bool)validation.Item1.DynamicInvoke(obj))
-                        errorList.Add(validation.Item2);
-                }
-            }            
-
-            var properties = Runtime.AttributeCache.Value.RetrieveFromType<T>(typeof(IValidator));
-
-            foreach (var prop in properties)
-            {
-                foreach (var attribute in prop.Value)
-                {
-                    var val = (IValidator)attribute;
-
-                    if (!val.IsValid(prop.Key.GetValue(obj, null)))
-                        errorList.Add($"{prop.Key.Name}: {val.ErrorMessage}");
-                }
-            }
+            var errorList = new ObjectValidationResult();
+            ValidateObject(obj, false, errorList.Add);
 
             return errorList;
         }
 
-        public bool IsValid<T>(T obj)
-        {
-            if (Equals(obj, null))
-                throw new ArgumentNullException(nameof(obj));
-            if (!_predicates.ContainsKey(typeof(T)))
-                throw new InvalidOperationException("There are no validators for this type");
-
-            if (_predicates.ContainsKey(typeof(T)))
-            {
-                foreach (var validation in _predicates[typeof(T)])
-                {
-                    if (!(bool)validation.Item1.DynamicInvoke(obj))
-                        return false;
-                }
-            }
-
-            var properties = Runtime.AttributeCache.Value.RetrieveFromType<T>(typeof(IValidator));
-
-            foreach (var prop in properties)
-            {
-                foreach (var attribute in prop.Value)
-                {
-                    var val = (IValidator)attribute;
-
-                    if (!val.IsValid(prop.Key.GetValue(obj, null)))
-                        return false;
-                }
-            }
-
-            return true;
-        }
+        /// <summary>
+        /// Returns true if ... is valid.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj">The object.</param>
+        /// <returns>
+        ///   <c>true</c> if the specified object is valid; otherwise, <c>false</c>.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">obj</exception>
+        /// <exception cref="InvalidOperationException">There are no validators for this type</exception>
+        public bool IsValid<T>(T obj) => ValidateObject(obj);
 
         /// <summary>
         /// Adds a validator to a specific class
         /// </summary>
         /// <typeparam name="T">The type of the object</typeparam>
         /// <param name="predicate">The predicate that will be evaluated</param>
+        /// <param name="message">The message.</param>
+        /// <exception cref="ArgumentNullException">
+        /// predicate
+        /// or
+        /// </exception>
         public void AddValidator<T>(Predicate<T> predicate, string message)
             where T : class
         {
@@ -101,11 +66,61 @@
 
             if (!_predicates.TryGetValue(typeof(T), out var existing))
             {
-                existing = new List<Tuple<Delegate,string>>();
+                existing = new List<Tuple<Delegate, string>>();
                 _predicates[typeof(T)] = existing;
             }
 
-            existing.Add(Tuple.Create((Delegate)predicate,message));
+            existing.Add(Tuple.Create((Delegate) predicate, message));
+        }
+
+        private bool ValidateObject<T>(T obj, bool returnOnError = true, Action<string, string> action = null)
+        {   
+            if (Equals(obj, null))
+                throw new ArgumentNullException(nameof(obj));
+           
+            if (_predicates.ContainsKey(typeof(T)))
+            {
+                foreach (var validation in _predicates[typeof(T)])
+                {
+                    if ((bool) validation.Item1.DynamicInvoke(obj)) continue;
+
+                    action?.Invoke(string.Empty, validation.Item2);
+                    if (returnOnError) return false;
+                }
+            }
+
+            var properties = Runtime.AttributeCache.Value.RetrieveFromType<T>(typeof(IValidator));
+
+            foreach (var prop in properties)
+            {
+                foreach (var attribute in prop.Value)
+                {
+                    var val = (IValidator) attribute;
+
+                    if (val.IsValid(prop.Key.GetValue(obj, null))) continue;
+
+                    action?.Invoke(prop.Key.Name, val.ErrorMessage);
+                    if (returnOnError) return false;
+                }
+            }
+
+            return true;
+        }
+    }
+
+    public class ObjectValidationResult
+    {
+        public List<ValidationError> Errors { get; set; }
+
+        public bool IsValid => !Errors.Any();
+
+        public void Add(string propertyName, string errorMessage) =>
+            Errors.Add(new ValidationError {ErrorMessage = errorMessage, PropertyName = errorMessage});
+
+        public class ValidationError
+        {
+            public string PropertyName { get; set; }
+            public string ErrorMessage { get; set; }
         }
     }
 }
