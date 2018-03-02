f5e8404dad7c531644e5985ff59cb9c085c42c18
Pablo Alcala <juan.alcala@unosquare.com>
2018-03-02 12:16:35 -0600

    Update test cases

--- test/Unosquare.Swan.Test/ObjectValidatorTest.cs
+++ test/Unosquare.Swan.Test/ObjectValidatorTest.cs
@@ -22,101 +22,105 @@
         }
 
         [Test]
-        public void ValidObject_ReturnsTrue()
+        public void ValidObject_ReturnsNoErrors()
         {
             var obj = new ObjectValidator();
             obj.AddValidator<SimpleValidationMock>(x => !string.IsNullOrEmpty(x.Name), "Name must not be empty");
-            var res = obj.Validate(new SimpleValidationMock { Name = "" });
+            var res = obj.Validate(new SimpleValidationMock { Name = "Name" });
 
-            Assert.IsTrue(res.Count > 0);
+            Assert.IsTrue(res.IsValid);
         }
 
         [Test]
-        public void InvalidObject_ReturnsFalse()
+        public void InvalidObject_ReturnsErrors()
         {
             var obj = new ObjectValidator();
-            var res = obj.Validate(new RangeMock { Age = 3 , Kilograms = 0 });
-            Assert.That(res.Count, Is.EqualTo(1));
+            var res = obj.Validate(new RangeMock { Age = 3, Kilograms = 0 });
+            Assert.IsFalse(res.IsValid);
+            Assert.That(res.Errors.Count, Is.EqualTo(1));
+        }
+    }
+
+    [TestFixture]
+    public class ObjectValidatorAttributes
+    {
+        [Test]
+        public void NullObject_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+            Runtime.ObjectValidator.Value.Validate(null as SimpleValidationMock));
+        }
+    }
+
+    [TestFixture]
+    public class NotNullAttribute
+    {        
+        public void NullProperty_ReturnsErrors()
+        {
+            var res = Runtime.ObjectValidator.Value.Validate(new NotNullMock());
+            Assert.IsFalse(res.IsValid);
+            Assert.That(res.Errors.Count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void NotNullProperty_ReturnsNoErrors()
+        {
+            var res = Runtime.ObjectValidator.Value.Validate(new NotNullMock { Number = 12 });
+            Assert.IsTrue(res.IsValid);
+            Assert.That(res.Errors.Count, Is.EqualTo(0));
+        }
+    }
+
+    [TestFixture]
+    public class RangeAttribute
+    {
+        [Test]
+        public void ValueWithinRange_ReturnsNoErrors()
+        {
+            var res = Runtime.ObjectValidator.Value.Validate(new RangeMock { Age = 5, Kilograms = 0.5 });
+            Assert.IsTrue(res.IsValid);
+        }
+
+        [Test]
+        public void ValueOutsideRange_ReturnsErrors()
+        {
+            var res = Runtime.ObjectValidator.Value.Validate(new RangeMock { Age = 0 });
+            Assert.IsFalse(res.IsValid);
+            Assert.That(res.Errors.Count, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void InvalidType_ReturnsErrors()
+        {
+            var res = Runtime.ObjectValidator.Value.Validate(new InvalidRangeMock { Invalid = "inv" });
+            Assert.IsFalse(res.IsValid);
+            Assert.That(res.Errors.Count, Is.EqualTo(1));
+        }
+    }
+
+    [TestFixture]
+    public class RegexAttribute
+    {
+        [TestCase("hi", true, 0)]
+        [TestCase("Hi", false, 1)]
+        public void StringValidation(string salute, bool valid, int count)
+        {
+            var res = Runtime.ObjectValidator.Value.Validate(new RegexMock { Salute = salute });
+            Assert.That(valid, Is.EqualTo(res.IsValid));
+            Assert.That(res.Errors.Count, Is.EqualTo(count));
+        }
+
+        [Test]
+        public void NullString_ReturnsErrors()
+        {
+            var res = Runtime.ObjectValidator.Value.Validate(new RegexMock { Salute = null });
+            Assert.IsFalse(res.IsValid);
+        }
+
+        [Test]
+        public void NotStringType_ThrowsInvalidOperationException()
+        {
+            Assert.Throws<InvalidOperationException>(() => Runtime.ObjectValidator.Value.Validate(new InvalidRegexMock { Salute = 1 }));
         }
     }
 }
-
-//    [TestFixture]
-//    public class ObjectValidatorAttributes
-//    {
-//        [Test]
-//        public void NullObject_ThrowsArgumentNullException()
-//        {
-//            Assert.Throws<ArgumentNullException>(() => ObjectValidator.IsValid(null as SimpleValidationMock));            
-//        }
-//    }
-
-//    [TestFixture]
-//    public class NotNullAttribute
-//    {
-//        [Test]
-//        public void NullProperty_ReturnsFalse()
-//        {
-//            var res = ObjectValidator.IsValid(new NotNullMock());
-//            Assert.IsFalse(res);
-//        }
-
-//        [Test]
-//        public void NotNullProperty_ReturnsTrue()
-//        {
-//            var res = ObjectValidator.IsValid(new NotNullMock { Number = 12});
-//            Assert.IsTrue(res);
-//        }
-//    }
-
-//    [TestFixture]
-//    public class RangeAttribute
-//    {
-//        [Test]
-//        public void ValueWithinRange_ReturnsTrue()
-//        {
-//            var res = ObjectValidator.IsValid(new RangeMock { Age = 5 , Kilograms = 0.5});
-//            Assert.IsTrue(res);
-//        }
-
-//        [Test]
-//        public void ValueOutsideRange_ReturnsFalse()
-//        {
-//            var res = ObjectValidator.IsValid(new RangeMock { Age = 0 });
-//            Assert.IsFalse(res);
-//        }
-
-//        [Test]
-//        public void InvalidType_ReturnsFalse()
-//        {
-//            var res = ObjectValidator.IsValid(new InvalidRangeMock { Invalid = "inv" });
-//            Assert.IsFalse(res);
-//        }
-//    }
-
-//    [TestFixture]
-//    public class RegexAttribute
-//    {
-//        [TestCase("hi", true)]
-//        [TestCase("Hi", false)]
-//        public void StringValidation(string salute,bool valid)
-//        {
-//            ObjectValidator.IsValid(new RegexMock());
-//            var res = ObjectValidator.IsValid(new RegexMock { Salute = salute });
-//            Assert.That(valid, Is.EqualTo(res));
-//        }
-
-//        [Test]
-//        public void NullString_ReturnsFalse()
-//        {
-//            var res = ObjectValidator.IsValid(new RegexMock { Salute = null });
-//            Assert.IsFalse(res);
-//        }
-
-//        [Test]
-//        public void NotStringType_ThrowsInvalidOperationException()
-//        {
-//            Assert.Throws<InvalidOperationException>(() => ObjectValidator.IsValid(new InvalidRegexMock { Salute = 1 }));
-//        }
-//    }
-//}
