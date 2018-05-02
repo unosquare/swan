21851fd43b5a633cf66ef37da6014afe5f6bcb68
Geo Perez <geovanni.perez@gmail.com>
2018-05-02 11:06:19 -0500

    Better Tokenizer exceptions

--- test/Unosquare.Swan.Test/TokenizerTest.cs
+++ test/Unosquare.Swan.Test/TokenizerTest.cs
@@ -8,35 +8,50 @@
     [TestFixture]
     public class TokenizerTest
     {
-        [TestCase("HOLA", new[] { "HOLA" },
+        [TestCase("HOLA", 
+            new[] { "HOLA" },
             new[] { TokenType.String })]
-        [TestCase("=\"HOLA\"", new[] { "HOLA" },
+        [TestCase("=\"HOLA\"", 
+            new[] { "HOLA" },
             new[] { TokenType.String })]
-        [TestCase("=1+1", new[] { "1", "+", "1" },
+        [TestCase("=1+1", 
+            new[] { "1", "+", "1" },
             new[] { TokenType.Number, TokenType.Operator, TokenType.Number })]
-        [TestCase("= 1 + 1", new[] { "1", "+", "1" },
+        [TestCase("= 1 + 1", 
+            new[] { "1", "+", "1" },
             new[] { TokenType.Number, TokenType.Operator, TokenType.Number })]
-        [TestCase("= -1 * 1", new[] { "-1", "*", "1" },
+        [TestCase("= -1 * 1", 
+            new[] { "-1", "*", "1" },
             new[] { TokenType.Number, TokenType.Operator, TokenType.Number })]
-        [TestCase("=1+1+1.1", new[] { "1", "+", "1", "+", "1.1" },
+        [TestCase("=1+1+1.1", 
+            new[] { "1", "+", "1", "+", "1.1" },
             new[] { TokenType.Number, TokenType.Operator, TokenType.Number, TokenType.Operator, TokenType.Number })]
-        [TestCase("=10 mod 10", new[] { "10", "mod", "10" },
+        [TestCase("=10 mod 10", 
+            new[] { "10", "mod", "10" },
             new[] { TokenType.Number, TokenType.Operator, TokenType.Number })]
-        [TestCase("=10 and -10", new[] { "10", "and", "-10" },
+        [TestCase("=10 and -10", 
+            new[] { "10", "and", "-10" },
             new[] { TokenType.Number, TokenType.Operator, TokenType.Number })]
-        [TestCase("=10 >= 10", new[] { "10", ">=", "10" },
+        [TestCase("=10 >= 10", 
+            new[] { "10", ">=", "10" },
             new[] { TokenType.Number, TokenType.Operator, TokenType.Number })]
-        [TestCase("=(1+1)*1", new[] { "(", "1", "+", "1", ")", "*", "1" },
+        [TestCase("=(1+1)*1", 
+            new[] { "(", "1", "+", "1", ")", "*", "1" },
             new[] { TokenType.Parenthesis, TokenType.Number, TokenType.Operator, TokenType.Number, TokenType.Parenthesis, TokenType.Operator, TokenType.Number })]
-        [TestCase("=First(1,1)", new[] { "first", "(", "1", "1", ")" },
+        [TestCase("=First(1,1)", 
+            new[] { "first", "(", "1", "1", ")" },
             new[] { TokenType.Function, TokenType.Parenthesis, TokenType.Number, TokenType.Number, TokenType.Parenthesis })]
-        [TestCase("=First(\"HOLA\",1)", new[] { "first", "(", "HOLA", "1", ")" },
+        [TestCase("=First(\"HOLA\",1)", 
+            new[] { "first", "(", "HOLA", "1", ")" },
             new[] { TokenType.Function, TokenType.Parenthesis, TokenType.String, TokenType.Number, TokenType.Parenthesis })]
-        [TestCase("=First(Second(\"HOLA\",1))", new[] { "first", "(", "second", "(", "HOLA", "1", ")", ")" },
+        [TestCase("=First(Second(\"HOLA\",1))", 
+            new[] { "first", "(", "second", "(", "HOLA", "1", ")", ")" },
             new[] { TokenType.Function, TokenType.Parenthesis, TokenType.Function, TokenType.Parenthesis, TokenType.String, TokenType.Number, TokenType.Parenthesis, TokenType.Parenthesis })]
-        [TestCase("=Global!PageNumber", new[] { "Global!PageNumber" },
+        [TestCase("=Global!PageNumber", 
+            new[] { "Global!PageNumber" },
             new[] { TokenType.Variable })]
-        [TestCase("=First(Global!PageNumber.Value,1)", new[] { "first", "(", "Global!PageNumber.Value", "1", ")" },
+        [TestCase("=First(Global!PageNumber.Value,1)", 
+            new[] { "first", "(", "Global!PageNumber.Value", "1", ")" },
             new[] { TokenType.Function, TokenType.Parenthesis, TokenType.Variable, TokenType.Number, TokenType.Parenthesis })]
         public void TokenizeExpression_ReturnsListOfTokens(string input, string[] tokens, TokenType[] tokensType)
         {
