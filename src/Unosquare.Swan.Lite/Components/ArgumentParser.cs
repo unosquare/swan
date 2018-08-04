3a4b3b5b37044c256d94dbdf3eb16ab9c81d58b1
Geo Perez <geovanni.perez@gmail.com>
2018-08-04 15:31:44 -0500

    WIP

--- src/Unosquare.Swan.Lite/Components/ArgumentParser.cs
+++ src/Unosquare.Swan.Lite/Components/ArgumentParser.cs
@@ -3,8 +3,6 @@ namespace Unosquare.Swan.Components
     using System;
     using System.Collections.Generic;
     using System.Linq;
-    using System.Reflection;
-    using Attributes;
 
     /// <summary>
     /// Provides methods to parse command line arguments.
