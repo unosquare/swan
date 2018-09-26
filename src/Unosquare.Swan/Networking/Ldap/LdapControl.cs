81e731991ebb51b6311b5f13cfb4bead7338969a
Geo Perez <geovanni.perez@gmail.com>
2018-09-26 14:52:27 -0500

    Remove LdapUrl

--- src/Unosquare.Swan/Networking/Ldap/LdapControl.cs
+++ src/Unosquare.Swan/Networking/Ldap/LdapControl.cs
@@ -161,393 +161,6 @@ namespace Unosquare.Swan.Networking.Ldap
         }
     }
 
-    /// <summary>
-    /// Encapsulates parameters of an Ldap URL query as defined in RFC2255.
-    /// An LdapUrl object can be passed to LdapConnection.search to retrieve
-    /// search results.
-    /// </summary>
-    /// <seealso cref="LdapConnection.Search"></seealso>
-    internal class LdapUrl
-    {
-        private const int DefaultPort = 389;
-        private const int DefaultSslPort = 636;
-
-        private int _port;
-
-        public LdapUrl(string url)
-        {
-            ParseUrl(url);
-        }
-
-        /// <summary>
-        /// Gets or sets the distinguised name.
-        /// </summary>
-        /// <value>
-        /// The dn.
-        /// </value>
-        public string DN { get; set; }
-
-        /// <summary>
-        /// Returns an array of attribute names specified in the URL.
-        /// </summary>
-        /// <value>
-        /// The attribute array.
-        /// </value>
-        public string[] AttributeArray { get; private set; }
-
-        /// <summary>
-        /// Returns the port number of the Ldap server in the URL.
-        /// </summary>
-        /// <value>
-        /// The port.
-        /// </value>
-        public int Port => _port == 0 ? DefaultPort : _port;
-
-        /// <summary>
-        ///     Returns the depth of search. It returns one of the following from
-        ///     LdapConnection: SCOPE_BASE, SCOPE_ONE, or SCOPE_SUB.
-        /// </summary>
-        /// <returns>
-        ///     The search scope.
-        /// </returns>
-        public LdapScope Scope { get; private set; } = LdapScope.ScopeBase;
-
-        /// <summary>
-        /// Returns true if the URL is of the type ldaps (Ldap over SSL, a predecessor
-        /// to startTls).
-        /// </summary>
-        /// <value>
-        ///   <c>true</c> if secure; otherwise, <c>false</c>.
-        /// </value>
-        public bool Secure { get; private set; }
-        
-        private string[] Extensions { get; set; }
-
-        private string Filter { get; set; }
-
-        private string Host { get; set; }
-
-        /// <summary>
-        /// Returns a valid string representation of this Ldap URL.
-        /// </summary>
-        /// <returns>
-        /// The string representation of the Ldap URL.
-        /// </returns>
-        public override string ToString()
-        {
-            var url = new StringBuilder(256)
-                .Append(Secure ? "ldaps://" : "ldap://")
-                .Append(Host);
-
-            // Port not specified
-            if (_port != 0)
-            {
-                url.Append(":" + _port);
-            }
-
-            if (DN == null && AttributeArray == null && Scope == LdapScope.ScopeBase && Filter == null &&
-                Extensions == null)
-            {
-                return url.ToString();
-            }
-
-            url.Append("/");
-            if (DN != null)
-            {
-                url.Append(DN);
-            }
-
-            if (AttributeArray == null && Scope == LdapScope.ScopeBase && Filter == null && Extensions == null)
-            {
-                return url.ToString();
-            }
-
-            // attributes
-            url.Append("?");
-            if (AttributeArray != null)
-            {
-                // should we check also for attrs != "*"
-                for (var i = 0; i < AttributeArray.Length; i++)
-                {
-                    url.Append(AttributeArray[i]);
-                    if (i < AttributeArray.Length - 1)
-                    {
-                        url.Append(",");
-                    }
-                }
-            }
-
-            if (Scope == LdapScope.ScopeBase && Filter == null && Extensions == null)
-            {
-                return url.ToString();
-            }
-
-            // scope
-            url.Append("?");
-            if (Scope != LdapScope.ScopeBase)
-            {
-                url.Append(Scope == LdapScope.ScopeOne ? "one" : "sub");
-            }
-
-            if (Filter == null && Extensions == null)
-            {
-                return url.ToString();
-            }
-
-            // filter
-            url.Append(Filter == null ? "?" : $"?{Filter}");
-
-            if (Extensions == null)
-            {
-                return url.ToString();
-            }
-
-            // extensions
-            url.Append("?");
-
-            if (Extensions == null) return url.ToString();
-
-            for (var i = 0; i < Extensions.Length; i++)
-            {
-                url.Append(Extensions[i]);
-                if (i < Extensions.Length - 1)
-                {
-                    url.Append(",");
-                }
-            }
-
-            return url.ToString();
-        }
-
-        private static string[] ParseList(string listStr, char delimiter, int listStart, int listEnd)
-        {
-            // Check for and empty string
-            if (listEnd - listStart < 1)
-            {
-                return null;
-            }
-
-            // First count how many items are specified
-            var itemStart = listStart;
-            int itemEnd;
-            var itemCount = 0;
-            while (itemStart > 0)
-            {
-                // itemStart == 0 if no delimiter found
-                itemCount += 1;
-                itemEnd = listStr.IndexOf(delimiter, itemStart);
-                if (itemEnd <= 0 || itemEnd >= listEnd)
-                    break;
-                itemStart = itemEnd + 1;
-            }
-
-            // Now fill in the array with the attributes
-            itemStart = listStart;
-            var list = new string[itemCount];
-            itemCount = 0;
-            while (itemStart > 0)
-            {
-                itemEnd = listStr.IndexOf(delimiter, itemStart);
-                if (itemStart > listEnd)
-                {
-                    break;
-                }
-
-                if (itemEnd < 0)
-                    itemEnd = listEnd;
-                if (itemEnd > listEnd)
-                    itemEnd = listEnd;
-                list[itemCount] = listStr.Substring(itemStart, itemEnd - itemStart);
-                itemStart = itemEnd + 1;
-                itemCount += 1;
-            }
-
-            return list;
-        }
-
-        private void ParseUrl(string url)
-        {
-            if (string.IsNullOrWhiteSpace(url))
-                throw new UriFormatException("LdapUrl: URL cannot be null");
-
-            var scanStart = 0;
-            var scanEnd = url.Length;
-
-            // Check if URL is enclosed by < & >
-            if (url[scanStart] == '<')
-            {
-                if (url[scanEnd - 1] != '>')
-                    throw new UriFormatException("LdapUrl: URL bad enclosure");
-                scanStart += 1;
-                scanEnd -= 1;
-            }
-
-            // Determine the URL scheme and set appropriate default port
-            if (url.StartsWith("URL:", StringComparison.OrdinalIgnoreCase))
-            {
-                scanStart += 4;
-            }
-
-            if (url.StartsWith("ldap://", StringComparison.OrdinalIgnoreCase))
-            {
-                scanStart += 7;
-                _port = DefaultPort;
-            }
-            else if (url.StartsWith("ldaps://", StringComparison.OrdinalIgnoreCase))
-            {
-                Secure = true;
-                scanStart += 8;
-                _port = DefaultSslPort;
-            }
-            else
-            {
-                throw new UriFormatException("LdapUrl: URL scheme is not ldap");
-            }
-
-            // Find where host:port ends and dn begins
-            var dnStart = url.IndexOf("/", scanStart, StringComparison.Ordinal);
-            var hostPortEnd = scanEnd;
-            var novell = false;
-            if (dnStart < 0)
-            {
-                dnStart = url.IndexOf("?", scanStart, StringComparison.Ordinal);
-                if (dnStart > 0)
-                {
-                    if (url[dnStart + 1] == '?')
-                    {
-                        hostPortEnd = dnStart;
-                        dnStart += 1;
-                        novell = true;
-                    }
-                    else
-                    {
-                        dnStart = -1;
-                    }
-                }
-            }
-            else
-            {
-                hostPortEnd = dnStart;
-            }
-
-            // Check for IPV6 "[ipaddress]:port"
-            int portStart;
-            if (url[scanStart] == '[')
-            {
-                var hostEnd = url.IndexOf(']', scanStart + 1);
-                if (hostEnd >= hostPortEnd || hostEnd == -1)
-                {
-                    throw new UriFormatException("LdapUrl: \"]\" is missing on IPV6 host name");
-                }
-
-                // Get host w/o the [ & ]
-                Host = url.Substring(scanStart + 1, hostEnd - (scanStart + 1));
-                portStart = url.IndexOf(":", hostEnd, StringComparison.Ordinal);
-                if (portStart < hostPortEnd && portStart != -1)
-                {
-                    // port is specified
-                    _port = int.Parse(url.Substring(portStart + 1, hostPortEnd - (portStart + 1)));
-                }
-            }
-            else
-            {
-                portStart = url.IndexOf(":", scanStart, StringComparison.Ordinal);
-
-                // Isolate the host and port
-                if (portStart < 0 || portStart > hostPortEnd)
-                {
-                    // no port is specified, we keep the default
-                    Host = url.Substring(scanStart, hostPortEnd - scanStart);
-                }
-                else
-                {
-                    // port specified in URL
-                    Host = url.Substring(scanStart, portStart - scanStart);
-                    _port = int.Parse(url.Substring(portStart + 1, hostPortEnd - (portStart + 1)));
-                }
-            }
-
-            scanStart = hostPortEnd + 1;
-            if (scanStart >= scanEnd || dnStart < 0)
-                return;
-
-            // Parse out the base dn
-            scanStart = dnStart + 1;
-            var attrsStart = url.IndexOf('?', scanStart);
-
-            DN = attrsStart < 0
-                ? url.Substring(scanStart, scanEnd - scanStart)
-                : url.Substring(scanStart, attrsStart - scanStart);
-
-            scanStart = attrsStart + 1;
-
-            // Wierd novell syntax can have nothing beyond the dn
-            if (scanStart >= scanEnd || attrsStart < 0 || novell)
-                return;
-
-            // Parse out the attributes
-            var scopeStart = url.IndexOf('?', scanStart);
-            if (scopeStart < 0)
-                scopeStart = scanEnd - 1;
-            AttributeArray = ParseList(url, ',', attrsStart + 1, scopeStart);
-            scanStart = scopeStart + 1;
-            if (scanStart >= scanEnd)
-                return;
-
-            // Parse out the scope
-            var filterStart = url.IndexOf('?', scanStart);
-            var scopeStr = filterStart < 0
-                ? url.Substring(scanStart, scanEnd - scanStart)
-                : url.Substring(scanStart, filterStart - scanStart);
-
-            if (string.IsNullOrWhiteSpace(scopeStr) ||
-                string.Equals(scopeStr, "base", StringComparison.OrdinalIgnoreCase))
-            {
-                Scope = LdapScope.ScopeBase;
-            }
-            else if (string.Equals(scopeStr, "one", StringComparison.OrdinalIgnoreCase))
-            {
-                Scope = LdapScope.ScopeOne;
-            }
-            else if (string.Equals(scopeStr, "sub", StringComparison.OrdinalIgnoreCase))
-            {
-                Scope = LdapScope.ScopeSub;
-            }
-            else
-            {
-                throw new UriFormatException("LdapUrl: URL invalid scope");
-            }
-
-            scanStart = filterStart + 1;
-            if (scanStart >= scanEnd || filterStart < 0)
-                return;
-
-            // Parse out the filter
-            scanStart = filterStart + 1;
-            var extStart = url.IndexOf('?', scanStart);
-
-            var filterStr = extStart < 0
-                ? url.Substring(scanStart, scanEnd - scanStart)
-                : url.Substring(scanStart, extStart - scanStart);
-
-            if (!filterStr.Equals(string.Empty))
-            {
-                Filter = filterStr; // Only modify if not the default filter
-            }
-
-            scanStart = extStart + 1;
-            if (scanStart >= scanEnd || extStart < 0)
-                return;
-
-            // Parse out the extensions
-            var end = url.IndexOf('?', scanStart);
-            if (end > 0)
-                throw new UriFormatException("LdapUrl: URL has too many ? fields");
-
-            Extensions = ParseList(url, ',', scanStart, scanEnd);
-        }
-    }
-
     internal class LdapResponse : LdapMessage
     {
         internal LdapResponse(RfcLdapMessage message)
@@ -558,96 +171,11 @@ namespace Unosquare.Swan.Networking.Ldap
         public string ErrorMessage => ((IRfcResponse) Message.Response).GetErrorMessage().StringValue();
 
         public string MatchedDN => ((IRfcResponse) Message.Response).GetMatchedDN().StringValue();
-
-        public string[] Referrals
-        {
-            get
-            {
-                var reference = ((IRfcResponse) Message.Response).GetReferral();
-
-                if (reference == null)
-                {
-                    return new string[0];
-                }
-
-                // convert RFC 2251 Referral to String[]
-                var size = reference.Size();
-                var referrals = new string[size];
-                for (var i = 0; i < size; i++)
-                {
-                    var refString = ((Asn1OctetString) reference.Get(i)).StringValue();
-
-                    try
-                    {
-                        // get the referral URL
-                        var urlRef = new LdapUrl(refString);
-                        if (urlRef.DN == null)
-                        {
-                            var origMsg = Asn1Object.RequestingMessage.Asn1Object;
-                            string dn;
-                            if ((dn = origMsg.RequestDn) != null)
-                            {
-                                urlRef.DN = dn;
-                                refString = urlRef.ToString();
-                            }
-                        }
-                    }
-                    catch (UriFormatException)
-                    {
-                        // Ignore
-                    }
-                    finally
-                    {
-                        referrals[i] = refString;
-                    }
-                }
-
-                return referrals;
-            }
-        }
-
-        public LdapStatusCode ResultCode
-        {
-            get
-            {
-                if (Message.Response is RfcSearchResultEntry ||
-                    (IRfcResponse) Message.Response is RfcIntermediateResponse)
-                {
-                    return LdapStatusCode.Success;
-                }
-
-                return (LdapStatusCode) ((IRfcResponse) Message.Response).GetResultCode().IntValue();
-            }
-        }
-
-        internal LdapException ResultException
-        {
-            get
-            {
-                LdapException ex = null;
-
-                switch (ResultCode)
-                {
-                    case LdapStatusCode.Success:
-                    case LdapStatusCode.CompareTrue:
-                    case LdapStatusCode.CompareFalse:
-                        break;
-                    case LdapStatusCode.Referral:
-                        var refs = Referrals;
-                        ex = new LdapException(
-                            "Automatic referral following not enabled",
-                            LdapStatusCode.Referral,
-                            ErrorMessage);
-                        ex.SetReferrals(refs);
-                        break;
-                    default:
-                        ex = new LdapException(ResultCode.ToString().Humanize(), ResultCode, ErrorMessage, MatchedDN);
-                        break;
-                }
-
-                return ex;
-            }
-        }
+        
+        public LdapStatusCode ResultCode => Message.Response is RfcSearchResultEntry ||
+                                            (IRfcResponse) Message.Response is RfcIntermediateResponse
+            ? LdapStatusCode.Success
+            : (LdapStatusCode) ((IRfcResponse) Message.Response).GetResultCode().IntValue();
 
         internal LdapException Exception { get; set; }
         
@@ -658,10 +186,19 @@ namespace Unosquare.Swan.Networking.Ldap
                 throw Exception;
             }
 
-            var ex = ResultException;
-            if (ex != null)
+            switch (ResultCode)
             {
-                throw ex;
+                case LdapStatusCode.Success:
+                case LdapStatusCode.CompareTrue:
+                case LdapStatusCode.CompareFalse:
+                    break;
+                case LdapStatusCode.Referral:
+                    throw new LdapException(
+                        "Automatic referral following not enabled",
+                        LdapStatusCode.Referral,
+                        ErrorMessage);
+                default:
+                    throw new LdapException(ResultCode.ToString().Humanize(), ResultCode, ErrorMessage, MatchedDN);
             }
         }
     }
@@ -720,14 +257,16 @@ namespace Unosquare.Swan.Networking.Ldap
     internal sealed class RfcBindRequest
         : Asn1Sequence, IRfcRequest
     {
+        private readonly sbyte[] _password;
         private static readonly Asn1Identifier Id = new Asn1Identifier(LdapOperation.BindRequest);
         
-        public RfcBindRequest(int version, string name, sbyte[] passwd)
+        public RfcBindRequest(int version, string name, sbyte[] password)
             : base(3)
         {
+            _password = password;
             Add(new Asn1Integer(version));
             Add(name);
-            Add(new RfcAuthenticationChoice(passwd));
+            Add(new RfcAuthenticationChoice(password));
         }
 
         public Asn1Integer Version
