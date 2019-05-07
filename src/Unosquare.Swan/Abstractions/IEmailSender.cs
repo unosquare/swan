e267c00ed295eed19b0297d75a3ef8ae614ed801
Geo Perez <1775792+geoperez@users.noreply.github.com>
2019-05-07 12:17:59 -0500

    Missing files

---
+++ src/Unosquare.Swan/Abstractions/IEmailSender.cs
@@ -0,0 +1,19 @@
+﻿namespace Unosquare.Swan.Abstractions
+{
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Represents an interface for e-mail sender.
+    /// </summary>
+    public interface IEmailSender
+    {
+        /// <summary>
+        /// Sends the message.
+        /// </summary>
+        /// <param name="recipient">The recipient.</param>
+        /// <param name="subject">The subject.</param>
+        /// <param name="message">The message.</param>
+        /// <returns><c>true</c> if the message was sent, otherwise <c>false</c>.</returns>
+        Task<bool> SendMessage(string recipient, string subject, string message);
+    }
+}
