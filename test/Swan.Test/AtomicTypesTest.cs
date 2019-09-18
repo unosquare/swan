dda5f143e766999c619fd543107c99a1d2fb56c0
Geo Perez <1775792+geoperez@users.noreply.github.com>
2019-09-18 15:37:25 -0500

    Code Style

--- test/Swan.Test/AtomicTypesTest.cs
+++ test/Swan.Test/AtomicTypesTest.cs
@@ -107,10 +107,10 @@
             }
 
             Task.WaitAll(
-                Task.Factory.StartNew(ExchangeTask),
-                Task.Factory.StartNew(ExchangeTask));
+                Task.Run(ExchangeTask),
+                Task.Run(ExchangeTask));
 
-            Assert.That(atomic.Value, Is.EqualTo(Companies.Value3));
+            Assert.Greater(2, (int) atomic.Value);
         }
     }
 }
