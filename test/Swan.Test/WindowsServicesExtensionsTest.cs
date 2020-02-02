28b9e66b03a5a45c45a4eba3d15898108fafd015
Geovanni Perez <1775792+geoperez@users.noreply.github.com>
2020-02-02 16:01:50 -0600

    Update WindowsServicesExtensionsTest.cs

--- test/Swan.Test/WindowsServicesExtensionsTest.cs
+++ test/Swan.Test/WindowsServicesExtensionsTest.cs
@@ -22,93 +22,12 @@ namespace Swan.Test
         }
 
         [Test]
-        public void RunInConsoleMode()
-        {
-            var service = new Mock<Services.ServiceBase>();
-            service.Object.RunInConsoleMode();
-            Assert.That(true);
-        }
-
-        [Test]
         public void RunInConsoleMode_NullServicesArray()
         {
             Services.ServiceBase[] nulls= null;
 
             Assert.Throws<ArgumentNullException>(() =>
                 nulls.RunInConsoleMode());
-        }
-
-        [Test]
-        public void RunInConsoleModeArray()
-        {
-            var fixture = new Fixture();
-            fixture.Customize(new AutoMoqCustomization());
-            var services = fixture.CreateMany<Services.ServiceBase>();
-
-            services.ToArray().RunInConsoleMode();
-            Assert.That(true);
-        }
-
-        [Test]
-        public void RunInConsoleModeOnStop()
-        {
-            var service = new Mock<Services.ServiceBase>();
-
-            service.Object.RunInConsoleMode();
-            service.Protected().Verify("OnStop", Times.Once());
-        }
-
-        [Test]
-        public void RunInConsoleModeOnStart()
-        {
-            var service = new Mock<Services.ServiceBase>();
-
-            service.Object.RunInConsoleMode();
-            service.Protected().Verify(
-                    "OnStart",
-                    Times.Once(), 
-                    new object[] { new string[] { },});
-        }
-
-        [Test]
-        public void RunInConsoleModeOnManyStop()
-        {
-            var size = new Random().Next(25);
-            var mocks = new List<Mock<Services.ServiceBase>>();
-            var services = new List<Services.ServiceBase>();
-
-            for (int i = 0; i < size; i++)
-            {
-                var fixture = new Fixture();
-                fixture.Customize(new AutoMoqCustomization());
-                mocks.Add(fixture.Freeze<Mock<Services.ServiceBase>>());
-                services.Add(fixture.Create<Services.ServiceBase>());
-            }
-
-            services.ToArray().RunInConsoleMode();
-            mocks.ForEach(x => x.Protected().Verify("OnStop", Times.Once()));
-        }
-
-        [Test]
-        public void RunInConsoleModeOnManyStart()
-        {
-            var size = new Random().Next(25);
-            var mocks = new List<Mock<Services.ServiceBase>>();
-            var services = new List<Services.ServiceBase>();
-
-            for (int i = 0; i < size; i++)
-            {
-                var fixture = new Fixture();
-                fixture.Customize(new AutoMoqCustomization());
-                mocks.Add(fixture.Freeze<Mock<Services.ServiceBase>>());
-                services.Add(fixture.Create<Services.ServiceBase>());
-            }
-
-            services.ToArray().RunInConsoleMode();
-            mocks.ForEach(x => x.Protected().Verify(
-                "OnStart",
-                Times.Once(),
-                new object[] { new string[] { }, }));
         }
     }
 }
