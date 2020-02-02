6edd624199cade8831a19df8480c7448c2bf8c46
Jose Correa <josemanuelcorrea.13@gmail.com>
2020-02-02 16:20:10 -0600

    Unit testing (#214)
    * merge master
    
    * AtomicDateTime
    
    * Atomic Tests
    
    * debuggerLog and CircularBuffer tests
    
    * ExtensionsDatesTests
    
    * ExceptionExtensions Tests
    
    * Circular Buffer tests
    
    * ViewModelBase Test
    
    * changing to autofixture
    
    * adding ThreadWrokerBase Test
    
    * removing ViewModelBase in Swan.Lite
    
    * removing CircularBuffer Test
    
    * code refactor
    
    * Update ExceptionExtensionsTest.cs
    
    * changing to new using var, C#8
    
    * fix csvWriter test
    
    * Update Extensions.WindowsServices.cs
    
    * Update AtomicTypesTest.cs
    
    * Update .travis.yml
    
    * fixing atomicDouble test
    
    * Update ConnectionTest.cs
    
    * Update ThreadWorkerBaseTest.cs
    
    * fix csproj
    
    * fixing Moq package
    
    * Update WindowsServicesExtensionsTest.cs
    
    Co-authored-by: Geovanni Perez <1775792+geoperez@users.noreply.github.com>

---
+++ test/Swan.Test/WindowsServicesExtensionsTest.cs
@@ -0,0 +1,33 @@
+﻿using Moq;
+using Moq.Protected;
+using NUnit.Framework;
+using System;
+using AutoFixture;
+using System.Linq;
+using AutoFixture.AutoMoq;
+using System.Collections.Generic;
+
+namespace Swan.Test
+{
+    [TestFixture]
+    public class WindowsServicesExtensionsTest
+    {
+        [Test]
+        public void RunInConsoleMode_NullService()
+        {
+            Services.ServiceBase nullObject = null;
+
+            Assert.Throws<ArgumentNullException>(() =>
+                nullObject.RunInConsoleMode());
+        }
+
+        [Test]
+        public void RunInConsoleMode_NullServicesArray()
+        {
+            Services.ServiceBase[] nulls= null;
+
+            Assert.Throws<ArgumentNullException>(() =>
+                nulls.RunInConsoleMode());
+        }
+    }
+}
