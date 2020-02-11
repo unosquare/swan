fa01b82c0fd2d8c8fb1f759268e5e57faa6642ab
Geo Perez <1775792+geoperez@users.noreply.github.com>
2020-02-11 11:16:29 -0600

    Fix namespace

--- src/Swan.Lite/Enums.cs
+++ src/Swan.Lite/Enums.cs
@@ -41,25 +41,4 @@
         /// </summary>
         Little,
     }
-
-    /// <summary>
-    /// Enumerates the JSON serializer cases to use: None (keeps the same case), PascalCase, or camelCase.
-    /// </summary>
-    public enum JsonSerializerCase
-    {
-        /// <summary>
-        /// The none
-        /// </summary>
-        None,
-
-        /// <summary>
-        /// The pascal case (eg. PascalCase)
-        /// </summary>
-        PascalCase,
-
-        /// <summary>
-        /// The camel case (eg. camelCase)
-        /// </summary>
-        CamelCase,
-    }
 }
---
+++ src/Swan.Lite/Formatters/JsonSerializerCase.cs
@@ -0,0 +1,23 @@
+﻿namespace Swan.Formatters
+{
+    /// <summary>
+    /// Enumerates the JSON serializer cases to use: None (keeps the same case), PascalCase, or camelCase.
+    /// </summary>
+    public enum JsonSerializerCase
+    {
+        /// <summary>
+        /// The none
+        /// </summary>
+        None,
+
+        /// <summary>
+        /// The pascal case (eg. PascalCase)
+        /// </summary>
+        PascalCase,
+
+        /// <summary>
+        /// The camel case (eg. camelCase)
+        /// </summary>
+        CamelCase,
+    }
+}
