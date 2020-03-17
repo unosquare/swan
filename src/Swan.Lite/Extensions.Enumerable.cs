1ed5ecdf1271135ede59a98d89ff89d9501e4d46
JCorrea13 <josemanuelcorrea.13@gmail.com>
2020-03-17 12:25:47 -0600

    UnionExcludingNull changes

---
+++ src/Swan.Lite/Extensions.Enumerable.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Swan
+{
+    /// <summary>
+    /// This class contains extensions methods for types implementing IEnumerable<T>
+    /// </summary>
+    public static class EnumerableExtensions
+    {
+        /// <summary>
+        /// This method returns the <see cref="System.Linq.Enumerable.Union"/>
+        /// of all non-null parameters.
+        /// </summary>
+        /// <returns> An <see cref="IEnumerable{T}"/> that contains the elements from non-null input sequences, excluding duplicates. </returns>
+        public static IEnumerable<T> UnionExcludingNulls<T>(this IEnumerable<T> @this, IEnumerable<T> second)
+        {
+            if (@this == null) return second ?? Enumerable.Empty<T>();
+            return second == null ? @this : @this.Union(second);
+        }
+    }
+}
