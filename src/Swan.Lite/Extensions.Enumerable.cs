2cbcda415c668fe6a967405e9617662dbe93caad
JCorrea13 <josemanuelcorrea.13@gmail.com>
2020-04-05 16:48:44 -0500

    XML documentation

--- src/Swan.Lite/Extensions.Enumerable.cs
+++ src/Swan.Lite/Extensions.Enumerable.cs
@@ -12,7 +12,12 @@ namespace Swan
         /// This method returns the <see cref="Enumerable.Union{TSource}">Union</see>
         /// of all non-null parameters.
         /// </summary>
-        /// <returns> An <see cref="IEnumerable{TSource}"/> that contains the elements from non-null input sequences, excluding duplicates. </returns>
+        /// <typeparam name="TSource">The type of the elements of the input sequences.</typeparam>
+        /// <param name="this">An IEnumerable&lt;TSource&gt; whose distinct elements forms the first set of the union.</param>
+        /// <param name="second">An IEnumerable&lt;TSource&gt; whose distinct elements forms the second set of the union.</param>
+        /// <returns>
+        /// An <see cref="IEnumerable{TSource}" /> that contains the elements from non-null input sequences, excluding duplicates.
+        /// </returns>
         public static IEnumerable<TSource> UnionExcludingNulls<TSource>(this IEnumerable<TSource> @this, IEnumerable<TSource> second)
             => Enumerable.Union(
                 @this ?? Enumerable.Empty<TSource>(),
@@ -22,7 +27,13 @@ namespace Swan
         /// This method returns the <see cref="Enumerable.Union{TSource}">Union</see>
         /// of all non-null parameters.
         /// </summary>
-        /// <returns> An <see cref="IEnumerable{TSource}"/> that contains the elements from non-null input sequences, excluding duplicates. </returns>
+        /// <typeparam name="TSource">The type of the elements of the input sequences.</typeparam>
+        /// <param name="this">An IEnumerable&lt;TSource&gt; whose distinct elements forms the first set of the union.</param>
+        /// <param name="second">An IEnumerable&lt;TSource&gt; whose distinct elements forms the second set of the union.</param>
+        /// <param name="comparer">The IEqualityComparer&lt;TSource&gt; to compare values.</param>
+        /// <returns>
+        /// An <see cref="IEnumerable{TSource}" /> that contains the elements from non-null input sequences, excluding duplicates.
+        /// </returns>
         public static IEnumerable<TSource> UnionExcludingNulls<TSource>(this IEnumerable<TSource> @this, IEnumerable<TSource> second, IEqualityComparer<TSource> comparer)
             => Enumerable.Union(
                 @this ?? Enumerable.Empty<TSource>(),
