b26886080031feeeb8e66894744089f55343ea70
JCorrea13 <josemanuelcorrea.13@gmail.com>
2020-04-04 20:38:48 -0600

    fixing the type parameter name

--- src/Swan.Lite/Extensions.Enumerable.cs
+++ src/Swan.Lite/Extensions.Enumerable.cs
@@ -4,7 +4,7 @@ using System.Linq;
 namespace Swan
 {
     /// <summary>
-    /// This class contains extension methods for types implementing IEnumerable&lt;T&gt;
+    /// This class contains extension methods for types implementing IEnumerable&lt;TSource&gt;
     /// </summary>
     public static class EnumerableExtensions
     {
@@ -12,10 +12,10 @@ namespace Swan
         /// This method returns the <see cref="System.Linq.Enumerable.Union"/>
         /// of all non-null parameters.
         /// </summary>
-        /// <returns> An <see cref="IEnumerable{T}"/> that contains the elements from non-null input sequences, excluding duplicates. </returns>
-        public static IEnumerable<T> UnionExcludingNulls<T>(this IEnumerable<T> @this, IEnumerable<T> second)
+        /// <returns> An <see cref="IEnumerable{TSource}"/> that contains the elements from non-null input sequences, excluding duplicates. </returns>
+        public static IEnumerable<TSource> UnionExcludingNulls<TSource>(this IEnumerable<TSource> @this, IEnumerable<TSource> second)
         {
-            if (@this == null) return second ?? Enumerable.Empty<T>();
+            if (@this == null) return second ?? Enumerable.Empty<TSource>();
             return second == null ? @this : @this.Union(second);
         }
     }
