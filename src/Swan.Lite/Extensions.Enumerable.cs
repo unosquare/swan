55782ad3040f0d2778835f9d44289a3c5bcc6be5
JCorrea13 <josemanuelcorrea.13@gmail.com>
2020-04-04 20:45:14 -0600

    adding overload with comparer

--- src/Swan.Lite/Extensions.Enumerable.cs
+++ src/Swan.Lite/Extensions.Enumerable.cs
@@ -17,5 +17,16 @@ namespace Swan
             => Enumerable.Union(
                 @this ?? Enumerable.Empty<TSource>(),
                 second ?? Enumerable.Empty<TSource>());
+
+        /// <summary>
+        /// This method returns the <see cref="Enumerable.Union{TSource}">Union</see>
+        /// of all non-null parameters.
+        /// </summary>
+        /// <returns> An <see cref="IEnumerable{TSource}"/> that contains the elements from non-null input sequences, excluding duplicates. </returns>
+        public static IEnumerable<TSource> UnionExcludingNulls<TSource>(this IEnumerable<TSource> @this, IEnumerable<TSource> second, IEqualityComparer<TSource> comparer)
+            => Enumerable.Union(
+                @this ?? Enumerable.Empty<TSource>(),
+                second ?? Enumerable.Empty<TSource>(),
+                comparer);
     }
 }
