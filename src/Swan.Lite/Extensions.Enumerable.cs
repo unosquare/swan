cc4b80fd0f98ce101158f58280409de3ef7da5ad
JCorrea13 <josemanuelcorrea.13@gmail.com>
2020-04-04 20:36:24 -0600

    escaping HTML characters

--- src/Swan.Lite/Extensions.Enumerable.cs
+++ src/Swan.Lite/Extensions.Enumerable.cs
@@ -4,7 +4,7 @@ using System.Linq;
 namespace Swan
 {
     /// <summary>
-    /// This class contains extension methods for types implementing IEnumerable<T>
+    /// This class contains extension methods for types implementing IEnumerable&lt;T&gt;
     /// </summary>
     public static class EnumerableExtensions
     {
