609992112e28b7e9b0f75d84c7c8797d8ebfb58d
Geovanni Perez <1775792+geoperez@users.noreply.github.com>
2022-08-27 12:16:11 -0500

    Add Coverage

--- test/Swan.Test/ExclusiveTimerTest.cs
+++ test/Swan.Test/ExclusiveTimerTest.cs
@@ -7,21 +7,6 @@ using Threading;
 public class ExclusiveTimerTest
 {
     [Test]
-    public void WithDefaultTimer_WaitsForOneIteration()
-    {
-        var i = 0;
-
-        using var timer = new ExclusiveTimer(() => i++, 0, 100);
-        Thread.Sleep(130);
-
-        Assert.GreaterOrEqual(i, 1, "First iteration");
-
-        Thread.Sleep(120);
-
-        Assert.GreaterOrEqual(i, 2, "Second iteration");
-    }
-
-    [Test]
     public void WithFutureDate_WaitUntilDate()
     {
         var futureDate = DateTime.UtcNow.AddSeconds(1);
