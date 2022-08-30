6e83b1d7d2ba5e64e0c2d6e6ca5a0ea7282f6145
Brian Armenta <brian.armenta@unosquare.com>
2022-08-30 11:45:03 -0500

    Generated Unit Tests for AsyncEnumrableExtensions

---
+++ test/Swan.Test/AsyncEnumerableExtensionsTest.cs
@@ -0,0 +1,48 @@
+﻿namespace Swan.Test;
+
+using NUnit.Framework;
+using Swan.Data.Extensions;
+
+[TestFixture]
+
+public class AsyncEnumerableExtensionsTest
+{
+    static async IAsyncEnumerable<int> FetchItems()
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            await Task.Delay(1000);
+            yield return i;
+        }
+    }
+
+    [Test]
+    public async Task GetAllItemsCountEqualsTen()
+    {
+        //Arrange
+        CancellationToken ct = new CancellationToken();
+        IAsyncEnumerable<int> items;
+
+        //Act
+        items = FetchItems();
+        var response = await items.ToListAsync(ct);
+
+        //Assert
+        Assert.IsTrue(response.Count == 10);
+    }
+
+    [Test]
+    public async Task GetAllItemsTakeFirst()
+    {
+        //Arrange
+        CancellationToken ct = new CancellationToken();
+        IAsyncEnumerable<int> items;
+
+        //Act
+        items = FetchItems();
+        var response = await items.FirstOrDefaultAsync(ct);
+
+        //Assert
+        Assert.IsTrue(response != null);
+    }
+}
