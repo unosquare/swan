5ba4a1861ad1909e7ea1863e244d6b86aa0bcb5d
Brian Armenta <brian.armenta@unosquare.com>
2022-09-05 19:45:48 -0500

    Addet Unit Test for CogeGenExtension Class

---
+++ test/Swan.Test/CodeGenExtensionTests.cs
@@ -0,0 +1,22 @@
+﻿namespace Swan.Test;
+
+using Microsoft.Data.Sqlite;
+using NUnit.Framework;
+using Swan.Data.Extensions;
+using static Swan.Test.Mocks.ProjectRecord;
+
+[TestFixture]
+public class CodeGenExtensionTest
+{
+    [Test]
+    public void GenPoco()
+    {
+        var conn = new SqliteConnection("Data Source=:memory:");
+        conn.TableBuilder<Project>("Projects").ExecuteDdlCommand();
+
+        var table = conn.Table<Project>("Projects");
+        string pocoCode = table.GeneratePocoCode();
+
+        Assert.IsTrue(pocoCode.Contains($"[Table(\"{table.TableName}\")]"));
+    }
+}
