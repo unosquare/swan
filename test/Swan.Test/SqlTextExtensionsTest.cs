a7bb65e859ec0405601876ec4ce90ce8c3d2336d
Brian Armenta <brian.armenta@unosquare.com>
2022-09-09 14:21:57 -0500

    Testing when CommandSource, Table or Table.Columns is null

--- test/Swan.Test/SqlTextExtensionsTest.cs
+++ test/Swan.Test/SqlTextExtensionsTest.cs
@@ -2,13 +2,26 @@
 
 using Microsoft.Data.Sqlite;
 using NUnit.Framework;
+using Swan.Data;
 using Swan.Data.Extensions;
+using Swan.Data.Schema;
 using static Swan.Test.Mocks.ProjectRecord;
 
 [TestFixture]
 
 public class SqlTextExtensionsTest
 {
+   
+
+    [Test]
+    public void SelectFieldsWhenCommandSourceIsNullThrowsException()
+    {
+        var fields = new string[] { };
+        CommandSource command = null;
+
+        Assert.Throws<ArgumentNullException>(() => command.Select(fields));
+    }
+
     [Test]
     public void AppendWordJustTheSELECTWordWhenFieldsIsEmpty()
     {
@@ -30,6 +43,15 @@ public class SqlTextExtensionsTest
     }
 
     [Test]
+    public void SelectTableWhenTableIsNullThrowsException()
+    {
+        IDbTableSchema table = null;
+        var conn = new SqliteConnection("Data Source=:memory:");
+
+        Assert.Throws<ArgumentNullException>(() => conn.BeginCommandText().Select(table).EndCommandText());
+    }
+
+    [Test]
     public void ProvidingATableCreatesASelectFieldsFromTableFormatCommanText()
     {
         var conn = new SqliteConnection("Data Source=:memory:");
@@ -40,6 +62,15 @@ public class SqlTextExtensionsTest
 
         Assert.AreEqual("SELECT [ProjectId], [Name], [ProjectType], [CompanyId], [IsActive], [StartDate], [EndDate], [ProjectScope] FROM [Projects]",
             command.CommandText);
+    }
+
+    [Test]
+    public void SelectColumnsWhenColumnsIsNullThrowsException()
+    {
+        IReadOnlyList<IDbColumnSchema> Columns = null;
+        var conn = new SqliteConnection("Data Source=:memory:");
+
+        Assert.Throws<ArgumentNullException>(() => conn.BeginCommandText().Select(Columns).EndCommandText());
     }
 
     [Test]
