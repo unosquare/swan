0f90869e6ab307fa1778296348d4e5b7516f7a04
Brian Armenta <brian.armenta@unosquare.com>
2022-09-20 21:01:04 -0500

    Added Tests for SortedList FindStartValue and AsyncLazy GetValueAsync

---
+++ test/Swan.Test/AsyncLazyTest.cs
@@ -0,0 +1,49 @@
+﻿namespace Swan.Test;
+
+using NUnit.Framework;
+using Swan.Gizmos;
+using Swan.Test.Mocks;
+
+[TestFixture]
+public class AsyncLazyTest
+{
+    static async Task<LargeObject> InitLargeObject()
+    { 
+        LargeObject large = new LargeObject(Thread.CurrentThread.ManagedThreadId);
+        return large;
+    }
+
+    static async void ThreadProc(object state)
+    {
+        var large = await  lazyLargeObject.GetValueAsync();
+
+        lock (large)
+        {
+            large.Data[0] = Thread.CurrentThread.ManagedThreadId;
+        }
+    }
+
+    static AsyncLazy<LargeObject> lazyLargeObject = null;
+
+    [Test]
+    public void WithLargObject_getsValueAsync()
+    {
+        lazyLargeObject = new AsyncLazy<LargeObject>(InitLargeObject);
+
+        // Create and start 3 threads, each of which uses LargeObject.
+        Thread[] threads = new Thread[3];
+        for (int i = 0; i < 3; i++)
+        {
+            threads[i] = new Thread(ThreadProc);
+            threads[i].Start();
+        }
+        
+        // Wait for all 3 threads to finish.
+        foreach (Thread t in threads)
+        {
+            t.Join();
+        }
+
+        Assert.IsTrue(lazyLargeObject.IsValueCreated);
+    }
+}
---
+++ test/Swan.Test/Mocks/LargeObject.cs
@@ -0,0 +1,16 @@
+﻿namespace Swan.Test.Mocks
+{
+    public class LargeObject
+    {
+        public int InitializedBy { get { return initBy; } }
+
+        int initBy = 0;
+        public LargeObject(int initializedBy)
+        {
+            initBy = initializedBy;
+            Console.WriteLine("LargeObject was created on thread id {0}.", initBy);
+        }
+
+        public long[] Data = new long[100000000];
+    }
+}
